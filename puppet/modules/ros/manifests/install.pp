144c9c6b9f4b691dd58c35ddcd3d044031afe332
Mark Pitchless <markpitchless@gmail.com>
2014-02-28 11:02:05 +0000

    Activate the right repos.

--- puppet/modules/ros/manifests/install.pp
+++ puppet/modules/ros/manifests/install.pp
@@ -8,6 +8,7 @@
 # Actions:
 # - Adds ros apt key.
 # - Adds ros repos to apt.
+# - Activates restricted, universe, multiverse
 # - Installs ros desktop
 # - Install rosinstall and wstool
 # - Does rosdep init
@@ -35,12 +36,21 @@ define ros::install(
     #ensure => $ensure,
   }
 
+  # Activates restricted, universe, multiverse
+  $deb_line_match_1 = '^#\(deb http:.*'
+  $deb_line_match_2 += '\(-updates\)\? \(restricted\|universe\|multiverse\)\)'
+  $deb_line_match = "${deb_line_match_1}${lsbdistcodename}${deb_line_match_2}"
+  exec { 'ros-activate-repos':
+    command => "/bin/sed -i\".bak-$(date +\'%Y%m%d-%H%M%S\')\" 's/$deb_line_match/\\1/' /etc/apt/sources.list",
+    onlyif  => "/bin/grep '$deb_line_match' /etc/apt/sources.list"
+  }
+
   apt::source { "ros":
     location    => "http://packages.ros.org/ros/ubuntu",
     repos       => "main",
     key         => 'B01FA116',
     include_src => false,
-    require     => Package['wget']
+    require     => [ Package['wget'], Exec['ros-activate-repos'] ]
   }
 
   package {
@@ -51,7 +61,7 @@ define ros::install(
 
   exec {'rosdep-init':
       command => '/usr/bin/rosdep init',
-      require => Package['ros-hydro-desktop-full'],
+      require => Package["$ros_package"],
       creates => '/etc/ros/rosdep/sources.list.d/20-default.list';
   }
 }
