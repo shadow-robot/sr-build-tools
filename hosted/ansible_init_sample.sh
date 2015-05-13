c7a3520a47da559f3fd41b4ec3722b0030627a0a
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-05-13 17:54:31 +0300

    Added ros_user rle

--- hosted/ansible_init_sample.sh
+++ hosted/ansible_init_sample.sh
@@ -11,6 +11,6 @@ git clone https://github.com/shadow-robot/sr-build-tools.git
 cd ./sr-build-tools
 git checkout F_hosted_build_support
 
-sudo ansible-playbook -i "localhost," -c local ~/workspace/sr-build-tools/ansible/docker_site.yml --tags "ros_install,ros_workspace"
+sudo ansible-playbook -v -i "localhost," -c local ~/workspace/sr-build-tools/ansible/docker_site.yml --tags "ros_install,ros_user,ros_workspace"
 
 
