be36a33591572b6b897a3abc15cdf8d365047d96
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-05-15 17:26:20 +0300

    Removed shell build files

--- hosted/run_unit_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source /opt/ros/indigo/setup.bash
-source ~/workspace/devel/setup.bash
-
-cd ~/workspace
-catkin_make run_tests
-
-mv -vt $SHIPPABLE_REPO_DIR/shippable/testresults/ ~/workspace/build/test_results/*
