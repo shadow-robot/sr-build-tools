56a21923bb1cbeeb47aaec07956efece7cffccc7
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-06-12 13:13:11 +0300

    Added codecov module and one shell script to run all

---
+++ bin/sr-run-ci-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export server_type=$1
+export tags_list=$2
+
+git clone https://github.com/shadow-robot/sr-build-tools.git -b F#111_code_style_module sr-build-tools
+sudo apt-get update
+sudo apt-get install python-dev libxml2-dev libxslt-dev python-pip lcov wget -y
+sudo pip install ansible gcovr
+./sr-build-tools/ansible
+
+if [ "shippable" == $server_type ]; then
+    sudo ansible-playbook -v -i "localhost," -c local docker_site.yml --tags "shippable,$tags_list" -e "shippable_repo_dir=$SHIPPABLE_REPO_DIR  shippable_is_pull_request=$PULL_REQUEST codecov_secure=$secure"
+else
+    echo Not supported server type $server_type
+fi
