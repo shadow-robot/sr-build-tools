7746ae178e8723be4e269b6d476281549163f413
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-06-14 14:45:49 +0300

    Fixed branch version in shell file

--- bin/sr-run-ci-build.sh
+++ bin/sr-run-ci-build.sh
@@ -3,7 +3,7 @@
 export server_type=$1
 export tags_list=$2
 
-git clone https://github.com/shadow-robot/sr-build-tools.git -b F#111_code_style_module sr-build-tools
+git clone https://github.com/shadow-robot/sr-build-tools.git -b F#126_server_cache_support sr-build-tools
 sudo apt-get update
 sudo apt-get install python-dev libxml2-dev libxslt-dev python-pip lcov wget -y
 sudo pip install ansible gcovr
