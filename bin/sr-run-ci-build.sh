dfd6f7dfd7a6c98890b72ef19a339ada86be0ea7
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-07-27 12:45:28 +0300

    Added Semaphore support roles.

--- bin/sr-run-ci-build.sh
+++ bin/sr-run-ci-build.sh
@@ -19,9 +19,23 @@ sudo apt-get update
 sudo apt-get install python-dev libxml2-dev libxslt-dev python-pip lcov wget -y
 sudo pip install ansible gcovr
 
-if [ "shippable" == $server_type ]; then
-    export extra_variables="shippable_repo_dir=$SHIPPABLE_REPO_DIR  shippable_is_pull_request=$PULL_REQUEST codecov_secure=$CODECOV_TOKEN"
-    sudo ansible-playbook -v -i "localhost," -c local docker_site.yml --tags "shippable,$tags_list" -e "$extra_variables"
-else
-    echo Not supported server type $server_type
-fi
+case $server_type in
+
+"shippable") echo "Shippable server"
+  export extra_variables="shippable_repo_dir=$SHIPPABLE_REPO_DIR  shippable_is_pull_request=$PULL_REQUEST codecov_secure=$CODECOV_TOKEN"
+  sudo ansible-playbook -v -i "localhost," -c local docker_site.yml --tags "shippable,$tags_list" -e "$extra_variables"
+  ;;
+
+"semaphore") echo "Semaphore server"
+  mkdir -p ~/workspace/src
+  export project_dir_name=$(basename $SEMAPHORE_PROJECT_DIR)
+  mv $SEMAPHORE_PROJECT_DIR ~/workspace/src
+  export new_project_dir=~/workspace/src/$project_dir_name
+  sudo apt-get remove mongodb-* -y
+  export extra_variables="semaphore_repo_dir=$new_project_dir  semaphore_is_pull_request=$PULL_REQUEST_NUMBER codecov_secure=$CODECOV_TOKEN"
+  sudo ansible-playbook -v -i "localhost," -c local docker_site.yml --tags "semaphore,$tags_list" -e "$extra_variables"
+  ;;
+
+*) echo "Not supported server type $server_type"
+  ;;
+esac
