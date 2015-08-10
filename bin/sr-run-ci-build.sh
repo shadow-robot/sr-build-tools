4ee70ee1ed33deb84e0d1eded8460b48ad06e9e7
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-08-10 18:43:11 +0300

    First version of the local builds

--- bin/sr-run-ci-build.sh
+++ bin/sr-run-ci-build.sh
@@ -7,23 +7,23 @@ export tags_list=$3
 export docker_image="andriyp/ubuntu-ros-indigo-build-tools"
 export build_tools_folder="$HOME/sr-build-tools"
 
-# Do not install all libraries for circle because we are using docker container directly
-if [ "circle" != $server_type ]; then
+# Do not install all libraries for circle and local run because we are using docker container directly
+if  [ "circle" != $server_type ] && [ "local" != $server_type ]; then
   sudo apt-get update
   sudo apt-get install python-dev libxml2-dev libxslt-dev python-pip lcov wget git -y
   sudo pip install ansible gcovr
-fi
 
-# Check in case of cached file system
-if [ -d $build_tools_folder ]; then
-  # Cached
-  cd $build_tools_folder
-  git pull origin "$toolset_branch"
-  cd ./ansible
-else
-  # No caching
-  git clone https://github.com/shadow-robot/sr-build-tools.git -b "$toolset_branch" $build_tools_folder
-  cd $build_tools_folder/ansible
+  # Check in case of cached file system
+  if [ -d $build_tools_folder ]; then
+    # Cached
+    cd $build_tools_folder
+    git pull origin "$toolset_branch"
+    cd ./ansible
+  else
+    # No caching
+    git clone https://github.com/shadow-robot/sr-build-tools.git -b "$toolset_branch" $build_tools_folder
+    cd $build_tools_folder/ansible
+  fi
 fi
 
 case $server_type in
@@ -56,6 +56,13 @@ case $server_type in
 
 "docker_hub") echo "Docker Hub"
   sudo ansible-playbook -v -i "localhost," -c local docker_site.yml --tags "docker_hub,$tags_list"
+  ;;
+
+"local") echo "Local run"
+  export local_repo_dir=$4
+  docker pull $docker_image
+  export extra_variables="local_repo_dir=/host$local_repo_dir "
+  docker run -w "/root/sr-build-tools/ansible" -v $HOME:/host:rw $docker_image  bash -c "git pull && git checkout $toolset_branch && sudo ansible-playbook -v -i \"localhost,\" -c local docker_site.yml --tags \"local,$tags_list\" -e \"$extra_variables\" "
   ;;
 
 *) echo "Not supported server type $server_type"
