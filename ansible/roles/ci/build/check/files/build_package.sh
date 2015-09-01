f5f168f89888edb8ca409f9d4d982819df399ba0
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-09-01 09:38:32 +0300

    Removed debug code

--- ansible/roles/ci/build/check/files/build_package.sh
+++ ansible/roles/ci/build/check/files/build_package.sh
@@ -7,7 +7,6 @@ export errors_file_name=$4
 
 source /opt/ros/$ros_release/setup.bash
 
-echo "Executing delete for $ros_workspace/build and $ros_workspace/devel"
 rm -rf $ros_workspace/build
 rm -rf $ros_workspace/devel
 
