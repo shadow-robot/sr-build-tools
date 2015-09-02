93b481419b72fe7ef1832186aaefbb921292baff
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-09-02 13:17:00 +0300

    Added common package role dependency for unit_tests role

---
+++ ansible/roles/ci/common/packages/files/not_repo_package_list.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+export ros_workspace=$1
+export repo_sources_path=$2
+
+export result=""
+export has_packages=0
+
+for workspace_file in $(find $ros_workspace -type f -name 'package.xml');
+do
+
+    export is_repo_file=0
+
+    for repo_file in $(find $repo_sources_path -type f -name 'package.xml');
+    do
+        if [ $repo_file == $workspace_file ]; then
+            export is_repo_file=1
+            break
+        fi
+    done
+
+    if [ $is_repo_file -eq 0 ]; then
+
+        if [ $has_packages -eq 1 ]; then
+            export result="$result;"
+        fi
+        export package_name=$(grep -e '<name>' $workspace_file | sed -e 's,.*<name>\([^<]*\)</name>.*,\1,g')
+        export result="$result$package_name"
+        export has_packages=1
+
+    fi
+done
+
+echo $result
