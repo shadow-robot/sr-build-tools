e0ce24ae2fda9063371e0849742bf1317e31d116
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-09-02 15:47:25 +0300

    Fixed circle ci issues

---
+++ ansible/roles/ci/common/packages/files/packages_list.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+export directory=$1
+
+export result="["
+export has_packages=0
+
+cd $directory
+
+for file in $(find -type f -name 'package.xml');
+do
+    if [ $has_packages -eq 1 ]; then
+        export result="$result,"
+    fi
+    export package_name=$(grep -e '<name>' $file | sed -e 's,.*<name>\([^<]*\)</name>.*,\1,g')
+    export result="$result \"$package_name\""
+    export has_packages=1
+done
+
+echo "$result]"
