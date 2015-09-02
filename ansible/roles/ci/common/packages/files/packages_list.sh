aab7c96ee2d3fab8e5601f343108dcb6bf1d36e5
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-09-02 16:50:01 +0300

    Added comments

--- ansible/roles/ci/common/packages/files/packages_list.sh
+++ ansible/roles/ci/common/packages/files/packages_list.sh
@@ -12,6 +12,7 @@ do
     if [ $has_packages -eq 1 ]; then
         export result="$result,"
     fi
+    # Extracting package name from package.xml file using sed
     export package_name=$(grep -e '<name>' $file | sed -e 's,.*<name>\([^<]*\)</name>.*,\1,g')
     export result="$result \"$package_name\""
     export has_packages=1
