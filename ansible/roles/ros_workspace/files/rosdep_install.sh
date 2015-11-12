ba48945c4cc122352f6407fe42a2bbbe0488bf3d
Ugo Cupcic <ugo@shadowrobot.com>
2015-11-12 11:08:55 +0100

    fixing rosdep permissions

--- ansible/roles/ros_workspace/files/rosdep_install.sh
+++ ansible/roles/ros_workspace/files/rosdep_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 source ${1}/devel/setup.bash
+rosdep fix-permissions
 rosdep update
+rosdep fix-permissions
 rosdep install --default-yes --all --ignore-src --skip-keys sr_ur_msgs --skip-keys cereal_port
