4280aa519f5a8d17dd8a0a4c7b103eebcfff224a
VahidAminZ <vahid@shadowrobot.com>
2016-03-30 16:44:01 +0100

    changed devel to devel_isolated

--- ansible/roles/ros_workspace/files/rosdep_install.sh
+++ ansible/roles/ros_workspace/files/rosdep_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source ${1}/devel/setup.bash
+source ${1}/devel_isolated/setup.bash
 rosdep update
 rosdep install --default-yes --all --ignore-src --skip-keys sr_ur_msgs --skip-keys cereal_port
