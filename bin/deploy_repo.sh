85cab7bcc601dee551005a8974c768e1e5264247
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2016-06-07 19:50:04 +0300

    Make script work faster

--- bin/deploy_repo.sh
+++ bin/deploy_repo.sh
@@ -27,6 +27,10 @@ case $key in
     GITHUB_PASSWORD="$2"
     shift
     ;;
+    -b|--branch)
+    GITHUB_BRANCH="$2"
+    shift
+    ;;
     -e|--external)
     EXTERNAL_ARGUMENTS="$2"
     shift
@@ -46,6 +50,13 @@ if [ -z "${REPOSITORY_NAME}" ]; then
     REPOSITORY_NAME="sr_interface"
 fi
 
+if [ -z "${GITHUB_BRANCH}" ]; then
+    GITHUB_BRANCH_URL_PART="trunk"
+else
+    GITHUB_BRANCH_URL_PART="branches/${GITHUB_BRANCH}"
+fi
+
+
 if [ -z "${GITHUB_PASSWORD}" ] && [ -n "${GITHUB_LOGIN}" ]; then
     echo "git user = ${GITHUB_LOGIN}"
     echo -n "${GITHUB_LOGIN}'s GitHub password:"
@@ -53,24 +64,30 @@ if [ -z "${GITHUB_PASSWORD}" ] && [ -n "${GITHUB_LOGIN}" ]; then
     echo
 fi
 
+REPOSITORY_URL="https://github.com//${REPOSITORY_OWNER}/${REPOSITORY_NAME}.git/${GITHUB_BRANCH_URL_PART}/deployment"
+
 if [ -n "${GITHUB_LOGIN}" ]; then
-    REPOSITORY_URL="https://${GITHUB_LOGIN}:${GITHUB_PASSWORD}@github.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}.git"
     if [ -z "${EXTERNAL_ARGUMENTS}" ]; then
         EXTERNAL_ARGUMENTS=" -l ${GITHUB_LOGIN} -p ${GITHUB_PASSWORD}"
     else
         EXTERNAL_ARGUMENTS="${EXTERNAL_ARGUMENTS} -l ${GITHUB_LOGIN} -p ${GITHUB_PASSWORD}"
     fi
-else
-    REPOSITORY_URL="https://github.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}.git"
 fi
 
 sudo apt-get update
-sudo apt-get install git -y
+sudo apt-get install subversion -y
 
 pushd /tmp
-git clone ${REPOSITORY_URL}
+
+rm -rf ./deployment
+
+if [ -n "${GITHUB_LOGIN}" ]; then
+    svn export --no-auth-cache -q ${REPOSITORY_URL} --username ${GITHUB_LOGIN} --password ${GITHUB_PASSWORD}
+else
+    svn export --no-auth-cache -q ${REPOSITORY_URL}
+ fi
+/tmp/deployment/ansible/deploy.sh ${EXTERNAL_ARGUMENTS}
+rm -rf ./deployment
+
 popd
 
-/tmp/${REPOSITORY_NAME}/deployment/ansible/deploy.sh ${EXTERNAL_ARGUMENTS}
-
-rm -rf /tmp/${REPOSITORY_NAME}
