5f867d0366fc0d1be0fb496f209d39099ee5ff4e
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2016-07-20 17:26:50 +0300

    Fixed directory path for script

--- ansible/roles/ci/deploy/build_debs/files/generate_ros_debian.sh
+++ ansible/roles/ci/deploy/build_debs/files/generate_ros_debian.sh
@@ -3,7 +3,11 @@
 set -e # fail on errors
 set -x # echo commands run
 
-export ros_release=${1:-indigo}
+
+export package_directory=$1
+export ros_release=${2:-indigo}
+
+pushd ${package_directory}
 
 source <(grep '^export\|^source' ~/.bashrc)
 
@@ -14,3 +18,5 @@ export git_revision_number=`git rev-list HEAD | wc -l`
 grep -rl --include "*.em" "@(change_version)" . | xargs sed -i "s/@(change_version)/@(change_version).${git_revision_number}/g"
 
 bloom-generate rosdebian --os-name ubuntu --os-version trusty --ros-distro ${ros_release} --process-template-files
+
+popd
