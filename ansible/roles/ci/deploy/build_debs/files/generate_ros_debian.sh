643fe445bdda2e2ea47ff148aa7c6439d0243396
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2016-07-20 15:59:25 +0300

    Added ability to set minor version as git revision number.

---
+++ ansible/roles/ci/deploy/build_debs/files/generate_ros_debian.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e # fail on errors
+#set -x # echo commands run
+
+export ros_release=${1:-indigo}
+
+source <(grep '^export\|^source' ~/.bashrc)
+
+bloom-generate rosdebian --os-name ubuntu --os-version trusty --ros-distro ${ros_release} --place-template-files
+
+export git_revision_number=`git rev-list HEAD | wc -l`
+
+grep -rl --include "*.em" "@(change_version)" . | xargs sed -i "s/@(change_version)/@(change_version).${git_revision_number}/g"
+
+bloom-generate rosdebian --os-name ubuntu --os-version trusty --ros-distro ${ros_release} --process-template-files
