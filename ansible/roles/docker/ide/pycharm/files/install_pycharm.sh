1d8db3541ae684011180fdaf507945fc0d5ca9b6
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2016-12-26 14:58:48 +0200

    Added sudo permissions

--- ansible/roles/docker/ide/pycharm/files/install_pycharm.sh
+++ ansible/roles/docker/ide/pycharm/files/install_pycharm.sh
@@ -6,7 +6,7 @@ set -e # fail on errors
 export PYCHARM_HOME=${1:-/etc/pycharm}
 
 wget https://download.jetbrains.com/python/pycharm-community-2016.2.3.tar.gz -O /tmp/pycharm.tar.gz
-mkdir ${PYCHARM_HOME}
+sudo mkdir ${PYCHARM_HOME}
 tar -xzvf /tmp/pycharm.tar.gz -C ${PYCHARM_HOME} --strip=1
 wget -P /tmp/ https://bootstrap.pypa.io/get-pip.py
-python /tmp/get-pip.py
+sudo python /tmp/get-pip.py
