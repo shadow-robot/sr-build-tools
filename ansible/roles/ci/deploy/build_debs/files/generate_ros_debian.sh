3f8090e3f2fd56b2772eb795a2af56cc5c30aa42
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2016-12-30 16:39:29 +0200

    Fixed debian build role

--- ansible/roles/ci/deploy/build_debs/files/generate_ros_debian.sh
+++ ansible/roles/ci/deploy/build_debs/files/generate_ros_debian.sh
@@ -5,17 +5,18 @@ set -e # fail on errors
 
 export package_directory=$1
 export ros_release=${2:-indigo}
+export ubuntu_version_name=${3:-trusty}
 
 pushd ${package_directory}
 
 source <(grep '^export\|^source' ~/.bashrc)
 
-bloom-generate rosdebian --os-name ubuntu --os-version trusty --ros-distro ${ros_release} --place-template-files
+bloom-generate rosdebian --os-name ubuntu --os-version ${ubuntu_version_name} --ros-distro ${ros_release} --place-template-files
 
 export git_revision_number=`git rev-list HEAD | wc -l`
 
 grep -rl --include "*.em" "@(change_version)" . | xargs sed -i "s/@(change_version)/@(change_version).${git_revision_number}/g"
 
-bloom-generate rosdebian --os-name ubuntu --os-version trusty --ros-distro ${ros_release} --process-template-files
+bloom-generate rosdebian --os-name ubuntu --os-version ${ubuntu_version_name} --ros-distro ${ros_release} --process-template-files
 
 popd
