4a78a7be86d9a5e839961980e7f1e48f2789a909
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2017-03-02 14:18:58 +0200

    Added test for simple YAML file

--- jenkins/job_manager/test/SettingsTest.groovy
+++ jenkins/job_manager/test/SettingsTest.groovy
@@ -1,7 +1,29 @@
 import org.junit.Test
-import static groovy.test.GroovyAssert.shouldFail
+import static org.mockito.Mockito.mock
 
 class SettingsTest {
+
+    @Test
+    void basicSettingsCheck() {
+        def loggerMock = mock(Logger.class)
+        def simpleSettingsYaml = '''\
+        settings:
+            ubuntu:
+                version: trusty
+            docker:
+                image: shadowrobot/ramcip
+                tag: main
+            ros:
+                release: indigo
+            toolset:
+                template_job_name: my_template
+            modules:
+                - check_cache
+                - code_coverage'''
+        def settings = new Settings(simpleSettingsYaml, loggerMock)
+
+        assert "my_template" == settings.settings.toolset.template_job_name
+    }
 
     @Test
     void indexOutOfBoundsAccess() {
