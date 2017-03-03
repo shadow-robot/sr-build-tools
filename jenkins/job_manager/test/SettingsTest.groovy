62a0879c257cdd128643365db1f91eeb52d64005
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2017-03-03 16:13:23 +0200

    Added more checks to basic settings

--- jenkins/job_manager/test/SettingsTest.groovy
+++ jenkins/job_manager/test/SettingsTest.groovy
@@ -3,6 +3,17 @@ import groovy.mock.interceptor.MockFor
 
 class SettingsTest {
 
+    void checkBasicSettings(Settings config) {
+        assert "trusty" == config.settings.ubuntu.version
+        assert "shadowrobot/build-tools" == config.settings.docker.image
+        assert "trusty-indigo" == config.settings.docker.tag
+        assert "indigo" == config.settings.ros.release
+        assert "my_template" == config.settings.toolset.template_job_name
+        assert 2 == config.settings.toolset.modules.size()
+        assert "check_cache" in config.settings.toolset.modules
+        assert "code_coverage" in config.settings.toolset.modules
+    }
+
     @Test
     void basicSettingsCheck() {
         def loggerMockContext = new MockFor(Logger)
@@ -23,14 +34,12 @@ class SettingsTest {
                     - check_cache
                     - code_coverage'''
         def config = new Settings(simpleSettingsYaml, loggerMock)
+        checkBasicSettings(config)
 
-        assert "trusty" == config.settings.ubuntu.version
-        assert "shadowrobot/build-tools" == config.settings.docker.image
-        assert "trusty-indigo" == config.settings.docker.tag
-        assert "indigo" == config.settings.ros.release
-        assert "my_template" == config.settings.toolset.template_job_name
-        assert 2 == config.settings.toolset.modules.size()
-        assert "check_cache" in config.settings.toolset.modules
-        assert "code_coverage" in config.settings.toolset.modules
+        def configForBranch = new Settings(simpleSettingsYaml, loggerMock, "my_super_feature")
+        checkBasicSettings(configForBranch)
+
+        def configForTrunk = new Settings(simpleSettingsYaml, loggerMock, "kinetic-devel")
+        checkBasicSettings(configForTrunk)
     }
 }
