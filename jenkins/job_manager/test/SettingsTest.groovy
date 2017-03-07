ba30e4d35267527b0b8578782c691148543ac0ec
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2017-03-07 19:19:41 +0200

    Added one more test to catch potential error

--- jenkins/job_manager/test/SettingsTest.groovy
+++ jenkins/job_manager/test/SettingsTest.groovy
@@ -142,4 +142,64 @@ class SettingsTest {
         def configForKineticTrunk = new Settings(branchInheritedSettingsYaml, loggerMock, "kinetic-devel")
         checkKineticTrunkSettings(configForKineticTrunk)
     }
+
+    void checkModulesListOverride() {
+        def branchOverridesModulesListSettingsYaml = '''\
+        settings:
+            ubuntu:
+                version: trusty
+            docker:
+                image: shadowrobot/build-tools
+                tag: trusty-indigo
+            ros:
+                release: indigo
+            toolset:
+                template_job_name: my_template
+                modules:
+                    - check_cache
+                    - code_coverage
+        trunks:
+            - name: indigo-devel
+            - name: kinetic-devel
+              settings:
+                  ubuntu:
+                      version: xenial
+                  ros:
+                      release: kinetic
+                  docker:
+                      tag: xenial-kinetic
+                  toolset:
+                      modules:
+                          - check_cache
+                          - all_tests
+        branch:
+            parent: kinetic-devel'''
+            settings:
+                toolset:
+                    modules:
+                        - check_cache
+                        - software_tests'''
+
+        def configDefault = new Settings(branchOverridesModulesListSettingsYaml, loggerMock)
+        checkBasicSettings(configDefault)
+
+        def configForBranch = new Settings(branchOverridesModulesListSettingsYaml, loggerMock, "my_new_kinetic_branch")
+        assert "xenial" == configForBranch.settings.ubuntu.version
+        assert "xenial-kinetic" == configForBranch.settings.docker.tag
+        assert "kinetic" == configForBranch.settings.ros.release
+        assert 2 == configForBranch.settings.toolset.modules.size()
+        assert "check_cache" in configForBranch.settings.toolset.modules
+        assert "software_tests" in configForBranch.settings.toolset.modules
+
+        def configForIndigoTrunk = new Settings(branchOverridesModulesListSettingsYaml, loggerMock, "indigo-devel")
+        checkBasicSettings(configForIndigoTrunk)
+
+        def configForKineticTrunk = new Settings(branchOverridesModulesListSettingsYaml, loggerMock, "kinetic-devel")
+        assert "xenial" == configForKineticTrunk.settings.ubuntu.version
+        assert "xenial-kinetic" == configForKineticTrunk.settings.docker.tag
+        assert "kinetic" == configForKineticTrunk.settings.ros.release
+        assert 2 == configForKineticTrunk.settings.toolset.modules.size()
+        assert "check_cache" in configForKineticTrunk.settings.toolset.modules
+        assert "all_tests" in configForKineticTrunk.settings.toolset.modules
+    }
 }
