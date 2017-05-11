5ca7dff3b89727381b092b94e28c0329ff853284
giusebar <giuseppe@shadowrobot.com>
2017-05-11 16:50:32 +0100

    add variable tag to playbook

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -134,7 +134,7 @@ else
     GITHUB_CREDENTIALS=" \"github_login\":\"${GITHUB_LOGIN}\", \"github_password\":\"${GITHUB_PASSWORD}\", "
 fi
 
-export MY_ANSIBLE_PARAMETERS="-vvv  --ask-become-pass ${PLAYBOOKS_DIR}/vagrant_site.yml"
+export MY_ANSIBLE_PARAMETERS="-vvv  --ask-become-pass ${PLAYBOOKS_DIR}/vagrant_site.yml --tags \"${TAGS_LIST}\""
 export EXTRA_ANSIBLE_PARAMETER_ROS_USER=" \"ros_user\":\"`whoami`\", \"ros_group\":\"`whoami`\", "
 
 echo ""
