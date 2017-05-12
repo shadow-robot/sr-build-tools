ac985d0c16153040f756694d0661aa2ac1be5adb
mikramarc <michal@shadowrobot.com>
2017-05-12 12:06:48 +0100

    Update entrypoint_x11.sh
    updated so it uses previous entrypoint

--- docker/ros/kinetic/x11/entrypoint_x11.sh
+++ docker/ros/kinetic/x11/entrypoint_x11.sh
@@ -3,19 +3,4 @@
 Xvfb :0 &
 export DISPLAY=:0
 
-USER_ID=${LOCAL_USER_ID:-9001}
-GROUP_ID=${LOCAL_GROUP_ID:-$USER_ID}
-
-export OLD_USER_ID=$(id -u $MY_USERNAME)
-export OLD_GROUP_ID=$(id -g $MY_USERNAME)
-
-usermod  --uid $USER_ID $MY_USERNAME
-groupmod --gid $GROUP_ID $MY_USERNAME
-
-export HOME=/home/$MY_USERNAME
-
-find $HOME -user $OLD_USER_ID -exec chown -h $USER_ID {} \;
-find $HOME -group $OLD_GROUP_ID -exec chgrp -h $GROUP_ID {} \;
-usermod -g $GROUP_ID $MY_USERNAME
-
-exec /usr/local/bin/gosu $MY_USERNAME "$@"
+/usr/local/bin/entrypoint.sh "$@"
