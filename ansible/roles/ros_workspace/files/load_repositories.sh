6220a81294962c457ffd48891b60a0693c8a59a0
mikramarc <michal@shadowrobot.com>
2017-06-01 16:57:04 +0100

    testing it

--- ansible/roles/ros_workspace/files/load_repositories.sh
+++ ansible/roles/ros_workspace/files/load_repositories.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 recursive_rosinstall () {
-    echo "current repo count: $current_repo_count"
+    echo "current repo count: $1"
     while [ $current_repo_count -ne $previous_repo_count ]; do
         find $current_folder -type f -name $rosinstall_filename -exec wstool merge -y {} \; 
         sed -i $1 .rosinstall
