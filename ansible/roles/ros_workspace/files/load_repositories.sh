de19d07fd8f14f0c7307deeab178aaf7cf2b97c0
mikramarc <michal@shadowrobot.com>
2017-06-01 14:16:18 +0100

    removed debugging code

--- ansible/roles/ros_workspace/files/load_repositories.sh
+++ ansible/roles/ros_workspace/files/load_repositories.sh
@@ -18,9 +18,6 @@ export current_repo_count=$(find $destination_folder -type f -name $rosinstall_f
 export previous_repo_count=-1
 export loops_count=$((levels_depth - 1))
 
-echo "Secure flag set to: ${secure}"
-echo "Initial workspace set to: ${initial_folder}"
-
 while [ $current_repo_count -ne $previous_repo_count ]; do
   if [ "${secure}" = true ]; then
       find $current_folder -type f -name $rosinstall_filename -exec wstool merge -y {} \; 
