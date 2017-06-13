4799776cb0f4ae5668391b608035b04ad12c9044
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2017-06-13 20:00:08 +0300

    Removed debug code

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -284,7 +284,7 @@ fi
 
 export WORKSPACE_SETTINGS="\"ros_workspace\":\"${WORKSPACE_PATH}\", \"ros_workspace_install\":\"${ROS_WORKSPACE_INSTALL_FILE}\" "
 export EXTERNAL_VARIABLES_JSON="{ ${GITHUB_CREDENTIALS} ${EXTRA_ANSIBLE_PARAMETER_ROS_USER} ${ROS_RELEASE_SETTINGS} ${X509_CLIENT_SETTINGS} ${WORKSPACE_SETTINGS} }"
-ansible-playbook -vvv ${MY_ANSIBLE_PARAMETERS} --extra-vars "${EXTERNAL_VARIABLES_JSON}"
+ansible-playbook ${MY_ANSIBLE_PARAMETERS} --extra-vars "${EXTERNAL_VARIABLES_JSON}"
 
 echo ""
 echo " ------------------------------------------------"
