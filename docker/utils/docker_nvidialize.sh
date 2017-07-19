b46195e61252f4d5921ccf468df3459e3574455e
mikramarc <michal@shadowrobot.com>
2017-07-19 10:42:10 +0100

    added script creating nvidia image from non-nvidia image

---
+++ docker/utils/docker_nvidialize.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+docker_image=$1
+
+mkdir -p /tmp/docker_nvidia_tmp
+cd /tmp/docker_nvidia_tmp && touch Dockerfile
+
+echo "FROM $docker_image" >> Dockerfile
+echo "LABEL com.nvidia.volumes.needed=\"nvidia_driver\"" >> Dockerfile
+echo "ENV PATH /usr/local/nvidia/bin:\${PATH}" >> Dockerfile
+echo "ENV LD_LIBRARY_PATH /usr/local/nvidia/lib:/usr/local/nvidia/lib64:\${LD_LIBRARY_PATH}" >> Dockerfile
+
+docker build --tag "$docker_image-nvidia" .
+
+cd && rm -rf /tmp/docker_nvidia_tmp
