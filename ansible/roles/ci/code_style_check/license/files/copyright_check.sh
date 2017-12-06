aa5b33625146465a3059e2e3a3c1a99e0422b023
mikramarc <michal@shadowrobot.com>
2017-12-06 10:15:04 +0000

    added licence check

---
+++ ansible/roles/ci/code_style_check/license/files/copyright_check.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export directory=$1
+cd $directory
+
+copyright_str="# Copyright"
+num_files_no_copyright=0
+
+for file_name in `find . -name "*.py" -type f`; do
+    # Ignoring __init__ and setup files
+    if [[ $file_name != *"__init__"* ]] && [[ $file_name != *"setup.py"* ]]; then
+        copyright_line=$(grep "$copyright_str" $file_name)
+        # echo $copyright_line
+        if [ "$copyright_line" == "" ]; then
+            if [ $num_files_no_copyright == 0 ]; then
+                echo "The following python files do not have copyright:"
+            fi
+            echo $file_name
+            let "num_files_no_copyright++"
+        fi
+    fi
+done
+if [ $num_files_no_copyright == 0 ]; then
+    echo "All files have copyright"
+    exit 0
+else
+    echo "There is a total of $num_files_no_copyright files without copyright"
+    exit 1
+fi
--- ansible/roles/ci/code_style_check/license/files/license_check.sh
+++ ansible/roles/ci/code_style_check/license/files/license_check.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 
-export directory=$1
-cd $directory
-
-copyright_str="# Copyright"
-num_files_no_copyright=0
-
-for file_name in `find . -name "*.py" -type f`; do
-    # Ignoring __init__ and setup files
-    if [[ $file_name != *"__init__"* ]] && [[ $file_name != *"setup.py"* ]]; then
-        copyright_line=$(grep "$copyright_str" $file_name)
-        # echo $copyright_line
-        if [ "$copyright_line" == "" ]; then
-            if [ $num_files_no_copyright == 0 ]; then
-                echo "The following python files do not have copyright:"
-            fi
-            echo $file_name
-            let "num_files_no_copyright++"
-        fi
-    fi
-done
-if [ $num_files_no_copyright == 0 ]; then
-    echo "All files have copyright"
-    exit 0
-else
-    echo "There is a total of $num_files_no_copyright files without copyright"
+if [ $(find . -name "LICENSE" | wc -l) == 0 ]; then
+    echo "No license file present"
     exit 1
+else
+    name=$(find . -name "LICENSE")
+    if [ $(wc -c < "$name") -le 235 ]; then
+        echo -e "License file present but content is different than expected. Please use the following text: \n"
+        echo -e "Copyright (C) 2017 Shadow Robot Company Ltd - All Rights Reserved."
+        echo -e "Redistribution and use in source and binary forms, with or without modification, are strictly prohibited by anyone without specific permission from Shadow Robot Company.\n"
+    else
+        echo "License file present"
+    fi
+    exit 0
 fi
