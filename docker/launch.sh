421586cbf82e2389afce56a4711cf4ac3bc52ce7
Beatriz Leon <beatriz@shadowrobot.com>
2018-02-19 16:19:33 -0500

    launch.sh

--- docker/launch.sh
+++ docker/launch.sh
@@ -35,6 +35,14 @@ case $key in
     LAUNCH_HAND="$2"
     shift
     ;;
+    -e|--ethercatinterface)
+    ETHERCAT_INTERFACE="$2"
+    shift
+    ;;
+    -b|--configbranch)
+    CONFIG_BRANCH="$2"
+    shift
+    ;;
     *)
     # ignore unknown option
     ;;
@@ -42,6 +50,7 @@ esac
 shift
 done
 
+# Default values
 if [ -z "${REINSTALL_DOCKER_CONTAINER}" ];
 then
     REINSTALL_DOCKER_CONTAINER=false
@@ -57,6 +66,11 @@ then
     LAUNCH_HAND=false
 fi
 
+if [ -z "${CONFIG_BRANCH}" ];
+then
+    CONFIG_BRANCH="kinetic-devel"
+fi
+
 echo "================================================================="
 echo "|                                                               |"
 echo "|             Shadow default docker deployment                  |"
@@ -70,11 +84,18 @@ echo "  * -p or --password Docker hub password"
 echo "  * -r or --reinstall flag to know if the docker container should be fully reinstalled (false by default)"
 echo "  * -n or --name name of the docker container"
 echo ""
-echo "example: ./launch.sh -i shadowrobot/dexterous-hand:indigo -n hand_e_indigo_real_hw -u mydockerhublogin -p mysupersecretpassword"
+echo "example hand E: ./launch.sh -i shadowrobot/dexterous-hand:indigo -n hand_e_indigo_real_hw -b "
+echo "example hand H: ./launch.sh -i shadowrobot/flexible-hand:kinetic-release -n hand_h_kinetic_real_hw -e enp0s25 -u mydockerhublogin -p mysupersecretpassword"
 echo ""
 echo "image name        = ${DOCKER_IMAGE_NAME}"
 echo "container name    = ${DOCKER_CONTAINER_NAME}"
 echo "reinstall flag    = ${REINSTALL_DOCKER_CONTAINER}"
+
+# From ANSI escape codes we have the following colours
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
 if [ -z ${DOCKER_IMAGE_NAME} ] || [ -z ${DOCKER_CONTAINER_NAME} ]; then
     echo "Docker image name and name of container are required"
@@ -86,69 +107,28 @@ HAND_H_NAME="flexible-hand"
 if echo "${DOCKER_IMAGE_NAME}" | grep -q "${HAND_E_NAME}"; then
     echo "Hand E/G image requested"
     HAND_H=false
+    # Check if config branch name was specified
+
 elif echo "${DOCKER_IMAGE_NAME}" | grep -q "${HAND_H_NAME}"; then
     echo "Hand H image requested"
     HAND_H=true
+    # Check if they have specified the ethercat interface
+    if [ -z ${ETHERCAT_INTERFACE} ] ; then
+        echo -e "${RED}Ethercat interface ID needs to be specified ${NC}"
+        exit 1
+    fi
 else
     echo "Unknown image requested"
     HAND_H=""
     exit 1
 fi
 
-# Setting up launch file
-if [ ${HAND_H} = true ]; then
-    LAUNCH_FILE = ""
-fi
-
-if [ ${DESKTOP_ICON} = true ] ; then
-    echo ""
-    echo " -------------------------------"
-    echo " |   Making desktop shortcut   |"
-    echo " -------------------------------"
-    echo ""
-
-    echo "Creating launcher folder"
-    APP_FOLDER=/home/$USER/launcher_app
-    if [ ! -d "${APP_FOLDER}" ]; then
-      mkdir ${APP_FOLDER}
-    fi
-
-    echo "Downloading the script"
-    # TODO: change this for master before merging
-    curl "https://raw.githubusercontent.com/shadow-robot/sr-build-tools/F%23SRC-1277_one_liner_docker_deployment/docker/launch.sh" >> ${APP_FOLDER}/launch.sh
-
-    echo "Creating executable file"
-    printf "#! /bin/bash
-    terminator -x bash -c 'cd ${APP_FOLDER}; ./launch.sh -i ${DOCKER_IMAGE_NAME} -n ${DOCKER_CONTAINER_NAME} -r false -d false; exec bash'" > ${APP_FOLDER}/launcher_exec.sh
-
-    echo "Downloading icon"
-    # TODO: change this for master before merging
-    wget https://github.com/shadow-robot/sr-build-tools/blob/F%23SRC-1277_one_liner_docker_deployment/docker/hand_h.png -P ${APP_FOLDER}
-
-    echo "Creating desktop file"
-    printf "[Desktop Entry]
-    Version=1.0
-    Name=Hand_Launcher
-    Comment=This is application launches the hand
-    Exec=/home/${USER}/launcher_app/launcher_exec.sh
-    Icon=/home/${USER}/launcher_app/hand_h.png
-    Terminal=false
-    Type=Application
-    Categories=Utility;Application;" > /home/$USER/Desktop/launcher.desktop
-
-    echo "Allowing files to be executable"
-    chmod +x ${APP_FOLDER}/launcher_exec.sh
-    chmod +x ${APP_FOLDER}/launch.sh
-    chmod +x /home/$USER/Desktop/launcher.desktop
-fi
-echo "finish"
+## Setting up launch file
+#if [ ${HAND_H} = true ]; then
+#    LAUNCH_FILE = ""
+#fi
 
 
-# From ANSI escape codes we have the following colours
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
 
 echo ""
 echo " -----------------------------------"
@@ -161,7 +141,7 @@ if [ -x "$(command -v docker)" ]; then
 else
     echo "Install docker"
     sudo apt-get update
-    sudo apt-get install \
+    sudo apt-get install  -y \
         apt-transport-https \
         ca-certificates \
         curl \
@@ -173,32 +153,37 @@ else
         stable"
 
     sudo apt-get update
-    sudo apt-get install docker-ce
+    sudo apt-get install -y docker-ce
 
     sudo groupadd docker
     sudo usermod -aG docker $USER
 fi
 
-echo "Docker log in "
-for i in 'seq 1 3';
-do
-    if [ -n ${DOCKER_HUB_USER} ]; then
-        if [ -n ${DOCKER_HUB_PASSWORD} ]; then
-            docker login --username ${DOCKER_HUB_USER} --password ${DOCKER_HUB_PASSWORD}
+# Log in to docker only for hand h images
+if [ ${HAND_H} = true ]; then
+    echo "Logging in to docker "
+    for i in 'seq 1 3';
+    do
+        if [ -z ${DOCKER_HUB_USER} ]; then
+            echo "Docker username not specified"
+            docker login
         else
-            docker login --username ${DOCKER_HUB_USER} --password-stdin
+            echo "Docker username specified"
+            if [ -z ${DOCKER_HUB_PASSWORD} ]; then
+                docker login --username ${DOCKER_HUB_USER} --password-stdin
+            else
+                docker login --username ${DOCKER_HUB_USER} --password ${DOCKER_HUB_PASSWORD}
+            fi
         fi
-    else
-        docker login
-    fi
-    if [ $? == 0 ]; then
-        break
-    fi
-    if [ ${i} == 3 and $? !=0 ]; then
-        echo -e "${RED}Docker login failed. You will not be able to pull private docker images.${NC}"
-        exit 1
-    fi
-done
+        if [ $? == 0 ]; then
+            break
+        fi
+        if [ ${i} == 3 and $? !=0 ]; then
+            echo -e "${RED}Docker login failed. You will not be able to pull private docker images.${NC}"
+            exit 1
+        fi
+    done
+fi
 
 if [ ${REINSTALL_DOCKER_CONTAINER} = false ] ; then
    echo "Not reinstalling docker image"
@@ -234,10 +219,53 @@ else
     docker run -it --privileged --name ${DOCKER_CONTAINER_NAME} --network=host -e DISPLAY -e QT_X11_NO_MITSHM=1 -e LOCAL_USER_ID=$(id -u) -v /tmp/.X11-unix:/tmp/.X11-unix:rw ${DOCKER_IMAGE_NAME}
 fi
 
-echo "Login out from docker"
-docker logout
+# If running for the first time create desktop shortcut
+if [ ${DESKTOP_ICON} = true ] ; then
+    echo ""
+    echo " -------------------------------"
+    echo " |   Making desktop shortcut   |"
+    echo " -------------------------------"
+    echo ""
 
-echo -e "${YELLOW}Please logout and login again.${NC}"
+    echo "Creating launcher folder"
+    APP_FOLDER=/home/$USER/launcher_app
+    if [ ! -d "${APP_FOLDER}" ]; then
+      mkdir ${APP_FOLDER}
+    fi
+
+    echo "Downloading the script"
+    # TODO: change this for master before merging
+    curl "https://raw.githubusercontent.com/shadow-robot/sr-build-tools/F%23SRC-1277_one_liner_docker_deployment/docker/launch.sh" >> ${APP_FOLDER}/launch.sh
+
+    echo "Creating executable file"
+    printf "#! /bin/bash
+    terminator -x bash -c 'cd ${APP_FOLDER}; ./launch.sh -i ${DOCKER_IMAGE_NAME} -n ${DOCKER_CONTAINER_NAME} -r false -d false; exec bash'" > ${APP_FOLDER}/launcher_exec.sh
+
+    echo "Downloading icon"
+    # TODO: change this for master before merging
+    wget https://raw.githubusercontent.com/shadow-robot/sr-build-tools/F%23SRC-1277_one_liner_docker_deployment/docker/hand_h.png -O ${APP_FOLDER}/hand_h.png
+
+    echo "Creating desktop file"
+    printf "[Desktop Entry]
+    Version=1.0
+    Name=Hand_Launcher
+    Comment=This is application launches the hand
+    Exec=/home/${USER}/launcher_app/launcher_exec.sh
+    Icon=/home/${USER}/launcher_app/hand_h.png
+    Terminal=false
+    Type=Application
+    Categories=Utility;Application;" > /home/$USER/Desktop/launcher.desktop
+
+    echo "Allowing files to be executable"
+    chmod +x ${APP_FOLDER}/launcher_exec.sh
+    chmod +x ${APP_FOLDER}/launch.sh
+    chmod +x /home/$USER/Desktop/launcher.desktop
+fi
+
+# TODO: do we still need to do this?
+# echo "Login out from docker"
+# docker logout
+# echo -e "${YELLOW}Please logout and login again.${NC}"
 
 echo ""
 echo -e "${GREEN} ------------------------------------------------${NC}"
