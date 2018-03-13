aa69ad26ca8254e1288030bd02938d69ef540cca
azompas <anastasios@shadowrobot.com>
2018-03-13 18:29:19 +0000

    test branch added

--- docker/launch.sh
+++ docker/launch.sh
@@ -246,7 +246,7 @@ function docker_login
 
 # If running for the first time create desktop shortcut
 APP_FOLDER=/home/$USER/.launcher_app
-BUILD_TOOLS_BRANCH=master
+BUILD_TOOLS_BRANCH=F_auto_close_anastasis
 if [ ${DESKTOP_ICON} = true ] ; then
     echo ""
     echo " -------------------------------"
