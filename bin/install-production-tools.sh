db4c30c434126060a4d3a6b24a0ed143b7922387
dg-shadow <dg@shadowrobot.com>
2018-03-14 10:39:37 +0000

    new shell script

--- bin/install-production-tools.sh
+++ bin/install-production-tools.sh
@@ -1,12 +1,35 @@
+BRANCH=master
+VERSION=kinetic
+
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    -b|--buildtoolsbranch)
+    BRANCH="$2"
+    shift # past argument
+    ;;
+    -v|--rosversion)
+    VERSION="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
 echo "Installing Eepromtool"
 cd /home/user/
-wget https://github.com/shadow-robot/sr-build-tools/raw/$(echo $toolset_branch | sed 's/#/%23/g')/bin/eepromtool
+wget https://github.com/shadow-robot/sr-build-tools/raw/$(echo $BRANCH | sed 's/#/%23/g')/bin/eepromtool
 chmod +x /home/user/eepromtool
 echo "Installing Gedit"
 apt-get -y install gedit
 
 echo "Installing Multiplot"
-apt-get -y install ros-${ros_release_name}-rqt-multiplot
+apt-get -y install ros-${VERSION}-rqt-multiplot
 
 echo "Installing Multiplot Configs"
 cd /home/user/
