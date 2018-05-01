3c16e2ab92601ecaabbc3cfac736da81794a6ea4
giusebar <giuseppe@shadowrobot.com>
2018-05-01 18:32:54 +0100

    fix some bugs

--- docker/utils/save_latest_ros_logs.sh
+++ docker/utils/save_latest_ros_logs.sh
@@ -3,17 +3,16 @@
 set -e # fails on errors
 #set -x # echo commands run
 
-echo You are about to save latest ros logs, please add a not with reasons
-
-export notes_from_user
+echo You are about to save latest ros logs, please add a note with reasons
+read notes_from_user
 
 container_name=$(sudo docker ps | awk '{if(NR>1) print $NF}')
 dir=ros_logs_$(date +%Y-%m-%d)
 timestamp=$(date +%Y-%m-%d-%T)
 
-mkdir -p $dir
-docker cp -L $container_name:home/user/.ros/log/latest $dir
+mkdir -p ~/$dir
+docker cp -L $container_name:home/user/.ros/log/latest ~/$dir
 
-mv $dir/latest $dir/ros_log_$timestamp
-echo $notes_from_user > $dir/ros_log_$timestamp/notes_from_user.txt
+mv ~/$dir/latest ~/$dir/ros_log_$timestamp
+echo $notes_from_user > ~/$dir/ros_log_$timestamp/notes_from_user.txt
 
