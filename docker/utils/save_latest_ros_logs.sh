3fe6cb9e4c927f4150958399bd96c669fbcb0652
giusebar <giuseppe@shadowrobot.com>
2018-05-10 16:44:00 +0100

    fix

--- docker/utils/save_latest_ros_logs.sh
+++ docker/utils/save_latest_ros_logs.sh
@@ -4,20 +4,25 @@ set -e # fails on errors
 #set -x # echo commands run
 
 GREEN='\033[0;32m'
+RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 echo You are about to save latest ros logs, please add a note with reasons
 read notes_from_user
 
 container_name=$(sudo docker ps | awk '{if(NR>1) print $NF}')
-dir=ros_logs_$(date +%Y-%m-%d)
-timestamp=$(date +%Y-%m-%d-%T)
+if [ ! -z "$container_name" ]; then
+    dir=ros_logs_$(date +%Y-%m-%d)
+    timestamp=$(date +%Y-%m-%d-%T)
 
-mkdir -p ~/$dir
-docker cp -L $container_name:home/user/.ros/log/latest ~/$dir
+    mkdir -p ~/$dir
+    docker cp -L $container_name:home/user/.ros/log/latest ~/$dir
 
-mv ~/$dir/latest ~/$dir/ros_log_$timestamp
-echo $notes_from_user > ~/$dir/ros_log_$timestamp/notes_from_user.txt
+    mv ~/$dir/latest ~/$dir/ros_log_$timestamp
+    echo $notes_from_user > ~/$dir/ros_log_$timestamp/notes_from_user.txt
 
-echo -e "${GREEN} Latest ROS Logs Saved! ${NC}"
-sleep 5
+    echo -e "${GREEN} Latest ROS Logs Saved in $dir! ${NC}"
+else
+    echo -e "${RED}There is no docker container running, please start a container to save logs${NC}"
+    exit 1
+fi
