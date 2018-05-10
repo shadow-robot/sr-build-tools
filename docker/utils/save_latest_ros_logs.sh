b063269402bd72310fb2ee975204de1c084a50cf
giusebar <giuseppe@shadowrobot.com>
2018-05-10 16:14:54 +0100

    Add some niceness to script

--- docker/utils/save_latest_ros_logs.sh
+++ docker/utils/save_latest_ros_logs.sh
@@ -3,6 +3,9 @@
 set -e # fails on errors
 #set -x # echo commands run
 
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 echo You are about to save latest ros logs, please add a note with reasons
 read notes_from_user
 
@@ -16,3 +19,5 @@ docker cp -L $container_name:home/user/.ros/log/latest ~/$dir
 mv ~/$dir/latest ~/$dir/ros_log_$timestamp
 echo $notes_from_user > ~/$dir/ros_log_$timestamp/notes_from_user.txt
 
+echo -e "${GREEN} Latest ROS Logs Saved! ${NC}"
+sleep 5
