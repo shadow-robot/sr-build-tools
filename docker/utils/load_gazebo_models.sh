58202266e460c55c675e62c6e69b49265b7c6e1e
Ethan Fowler <ethan@shadowrobot.com>
2018-06-01 14:48:55 +0100

    Fixing formatting and adding documentation to gazebo model loader script.

--- docker/utils/load_gazebo_models.sh
+++ docker/utils/load_gazebo_models.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-# Script is based on http://machineawakening.blogspot.com/2015/05/how-to-download-all-gazebo-models.html
+# Script is based on http://machineawakening.blogspot.com/2015/05/how-to-download-all-gazebo-models.html. It is improved
+# in several ways. It avoids downloading all files and discarding unwanted ones by parsing the gazebo model manifest.
+# It allows the specification of models to fetch, e.g.:
+# ./load_gazebo_modles.sh ground_plane sun ambulance
+# These two changes make it much faster and less of a load on the server. For backwards compatibility, no arguments
+# results in all models being fetched.
 
 tmp_dir="/tmp/models.gazebosim.org"
 mkdir -p $tmp_dir
