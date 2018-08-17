8728a3124d355f4fa0090ba4cdc081424fbac2e0
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2018-08-17 15:41:32 +0100

    Update s3_upload_email_lambda_function.py

--- docker/utils/s3_upload_email_lambda_function.py
+++ docker/utils/s3_upload_email_lambda_function.py
@@ -16,11 +16,10 @@ def lambda_handler(event, context):
     eventtime = event['Records'][0]['eventTime']
     objectname = event['Records'][0]['s3']['object']['key']
     customername = objectname.split("/")[1].split("_")[0]
-    timestamp = objectname.split("/")[1].split("_")[1].split(".")[0].replace("%3A ",":")
+    timestamp = objectname.split("/")[1].split("_")[1].split(".")[0].replace("%3A",":")
     filename = objectname.split("/")[1]
     size =  str(event['Records'][0]['s3']['object']['size']/1024.0/1024.0)
     subjectline = "New ROS Logs upload for Shadow from "+customername
-    
     email_text = (
         f"New ROS Logs upload for Shadow\n"
         f"Customer: "+customername+"\n"
