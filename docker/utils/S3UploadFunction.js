34033abb5068b6213ba665d468bf229ba3f8d394
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2018-08-21 16:24:02 +0100

    Update S3UploadFunction.js

--- docker/utils/S3UploadFunction.js
+++ docker/utils/S3UploadFunction.js
@@ -1,9 +1,20 @@
 const AWS = require("aws-sdk"); const uuid = require("uuid");
 const sts = new AWS.STS(); const roleToAssume = process.env.role; const bucket = process.env.bucket;
+const apigateway = new AWS.APIGateway();
+
 exports.handler = (event, context, callback) => {
-  // console.log('event:', event);
-  // console.log('context:', context);
   let apiKeyId = event.requestContext.identity.apiKeyId;
+  let customername = "unknown customer"
+  
+  var params = {
+    apiKey: apiKeyId,
+    includeValue: true
+  };
+  apigateway.getApiKey(params, function(err, data) {
+    if (err) customername="error getting customer" // an error occurred
+    else     customername=data.name;           // successful response
+  });
+
   let sessionName = uuid.v4();
     sts.assumeRole({
       RoleArn: roleToAssume,
@@ -15,6 +26,7 @@ exports.handler = (event, context, callback) => {
       result+=`SESSION_TOKEN=${assumed.Credentials.SessionToken}\n`;
       result+=`EXPIRATION=${new Date(assumed.Credentials.Expiration).getTime()}\n`;
       result+=`URL=s3://${bucket}/${assumed.AssumedRoleUser.AssumedRoleId}/\n`;
+      result+=`CUSTOMER_NAME=${customername}\n`;
       callback(null, {
         statusCode: 200,
         body: result
