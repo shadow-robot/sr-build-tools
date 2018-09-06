1f5d631a854e11f2a702eeda9003638d7e29c7c6
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2018-09-06 19:13:26 +0300

    Added optimization of UIDs changes

--- docker/ros/kinetic/base/entrypoint.sh
+++ docker/ros/kinetic/base/entrypoint.sh
@@ -7,17 +7,24 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 GROUP_ID=${LOCAL_GROUP_ID:-$USER_ID}
 
-export OLD_USER_ID=$(id -u $MY_USERNAME)
-export OLD_GROUP_ID=$(id -g $MY_USERNAME)
-
-usermod  --uid $USER_ID $MY_USERNAME
-groupmod --gid $GROUP_ID $MY_USERNAME
-
 export HOME=/home/$MY_USERNAME
 
-find $HOME -user $OLD_USER_ID -exec chown -h $USER_ID {} \;
-find $HOME -group $OLD_GROUP_ID -exec chgrp -h $GROUP_ID {} \;
-usermod -g $GROUP_ID $MY_USERNAME
+export OLD_USER_ID=$(id -u $MY_USERNAME)
+
+if [ $OLD_USER_ID -ne $USER_ID ]
+then
+  usermod  --uid $USER_ID $MY_USERNAME
+  find $HOME -user $OLD_USER_ID -exec chown -h $USER_ID {} \;
+fi
+
+export OLD_GROUP_ID=$(id -g $MY_USERNAME)
+
+if [ $OLD_GROUP_ID -ne $GROUP_ID ]
+then
+  groupmod --gid $GROUP_ID $MY_USERNAME
+  find $HOME -group $OLD_GROUP_ID -exec chgrp -h $GROUP_ID {} \;
+  usermod -g $GROUP_ID $MY_USERNAME
+fi
 
 if mkdir -p /home/$MY_USERNAME/.ros/log/core_dumps ; then
     chown -R $MY_USERNAME:$MY_USERNAME /home/$MY_USERNAME/.ros/log/core_dumps
