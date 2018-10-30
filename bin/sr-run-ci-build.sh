6a8c561f39b503062e58cae38a09a06e127a4c9e
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2018-10-30 16:28:22 +0000

    Update sr-run-ci-build.sh

--- bin/sr-run-ci-build.sh
+++ bin/sr-run-ci-build.sh
@@ -127,6 +127,10 @@ case $server_type in
   export extra_variables="$extra_variables local_benchmarking_dir=$benchmarking_dir"
   docker run -w "$docker_user_home/sr-build-tools/ansible" -e LOCAL_USER_ID=$(id -u) $docker_flags --rm -v $HOME:/host:rw $docker_image  bash -c "git pull && git checkout $toolset_branch && git pull && PYTHONUNBUFFERED=1 ansible-playbook -v -i \"localhost,\" -c local docker_site.yml --tags \"local,$tags_list\" -e \"$extra_variables\" "
   ;;
+  
+"aws") echo "AWS CodeBuild using Docker Image from Docker Hub"
+  git pull && git checkout $toolset_branch && git pull && sudo PYTHONUNBUFFERED=1 ansible-playbook -vvv -i "localhost," -c local docker_site.yml --tags "local,check_cache,code_coverage,$tags_list" -e "$extra_variables"
+  ;;
 
 *) echo "Not supported server type $server_type"
   ;;
