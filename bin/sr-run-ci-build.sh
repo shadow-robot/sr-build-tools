ea8f37d25dfebb6447b7ecfd5985fc7fdd05a22f
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2018-10-31 11:05:38 +0000

    Update sr-run-ci-build.sh

--- bin/sr-run-ci-build.sh
+++ bin/sr-run-ci-build.sh
@@ -129,6 +129,25 @@ case $server_type in
   ;;
   
 "local-docker") echo "Using Docker Image from Docker Hub"
+  export local_repo_dir=$4
+  if [ -z "$unit_tests_result_dir" ]
+  then
+    export unit_tests_dir="$local_repo_dir/workspace/test_results"
+  else
+    export unit_tests_dir=$unit_tests_result_dir
+  fi
+  if [ -z "$coverage_tests_result_dir" ]
+  then
+    export coverage_tests_dir="$local_repo_dir/workspace/coverage_results"
+  else
+    export coverage_tests_dir=$coverage_tests_result_dir
+  fi
+  if [ -z "$benchmarking_result_dir" ]
+  then
+    export benchmarking_dir="$local_repo_dir/workspace/benchmarking_results"
+  else
+    export benchmarking_dir=$benchmarking_result_dir
+  fi
   export extra_variables="$extra_variables local_repo_dir=$local_repo_dir local_test_dir=$unit_tests_dir local_code_coverage_dir=$coverage_tests_dir"
   export extra_variables="$extra_variables local_benchmarking_dir=$benchmarking_dir"
   git pull && git checkout $toolset_branch && git pull && sudo PYTHONUNBUFFERED=1 ansible-playbook -vvv -i "localhost," -c local docker_site.yml --tags "local,check_cache,code_coverage,$tags_list" -e "$extra_variables"
