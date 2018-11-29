66d117555a66a5885d1467d88ba7d84a5491978d
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2018-11-29 03:36:22 +0000

    Update central_codebuild_creator.py

--- aws/central_codebuild_creator.py
+++ aws/central_codebuild_creator.py
@@ -48,14 +48,14 @@ for project_name in list_of_project_names:
 for repo_line in list_of_repos_text.splitlines():
     if (repo_line.startswith("  - ")):
         repo_name = repo_line.strip()[2:]
-        repo_aws_yml_master_branch = "master"
+        repo_aws_yml_master_branch = "F%23SRC-2345_setup_aws_build_of_build-servers-check"
         repo_aws_yml_url = "https://raw.githubusercontent.com/shadow-robot/build-servers-check/"+repo_aws_yml_master_branch+"/aws.yml"
         repo_aws_yml_response = requests.get(repo_aws_yml_url, auth=(git_username_dec,git_token_dec))
-        if (repo_aws_yml_response == '200'):
+        if (str(repo_aws_yml_response) == "<Response [200]>"):
             repo_aws_yml_text = list_of_repos_response.text
             status_text += "got this aws.yml text from "+repo_name+":" +repo_aws_yml_text+"\n"
         else:
-            status_text += repo_name+" does not have aws.yml in "+repo_aws_yml_master_branch+" branch" +"\n"+str(repo_aws_yml_response)+"\n"
+            status_text += repo_name+" does not have aws.yml in "+repo_aws_yml_master_branch+" branch" +"\n"
             if repo_name in dict_repo_projects:
                 project_candidates_for_deletion = dict_repo_projects[repo_name]
                 status_text += "project candidates for deletion: "+ ', '.join(project_candidates_for_deletion)+"\n"
