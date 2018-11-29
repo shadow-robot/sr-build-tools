8dbb911e035abed65d6dc5e67c0e7ca5233323c4
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2018-11-29 01:00:15 +0000

    Update central_codebuild_creator.py

--- aws/central_codebuild_creator.py
+++ aws/central_codebuild_creator.py
@@ -27,43 +27,36 @@ codebuildclient = boto3.client('codebuild')
 
 list_of_repos_url = "https://raw.githubusercontent.com/shadow-robot/sr-build-tools-internal/F%23SRC-2474_central_AWS_script/aws/configuration.yml"
 
-def lambda_handler(event, context):
+subjectline = "Automatic trigger for central_codebuild_creator"
     
-    subjectline = "Automatic trigger for central_codebuild_creator"
+#process repo_list one by one
     
-    #process repo_list one by one
+status_text = ""
     
-    status_text = ""
+list_of_repos_response = requests.get(list_of_repos_url, auth=(git_username_dec,git_token_dec))
+list_of_repos_text = list_of_repos_response.text
     
-    #get list of repos from sr-build-tools-internal
-    #get git token
+status_text = list_of_repos_text
+
+codebuildresponse = codebuildclient.list_projects(
+    sortBy='NAME',
+    sortOrder='ASCENDING'
+)
+
+list_of_project_names = codebuildresponse['projects']
     
-    list_of_repos_response = requests.get(list_of_repos_url, auth=(git_username_dec,git_token_dec))
-    list_of_repos_text = list_of_repos_response.text
-    
-    status_text = list_of_repos_text
-    
-    #get real list of CodeBuild projects
-    codebuildresponse = codebuildclient.list_projects(
-            sortBy='NAME',
-            sortOrder='ASCENDING'
-        )
-    list_of_project_names = codebuildresponse['projects']
-    
-    for repo in list_of_repos.split(","):
-        #check if it was aws.yml in the root or not
-        
-        build_project_name = build_project_name_start+repo
-        #check if this build project exists
-        if build_project_name in list_of_project_names:
-            status_text += "project found! : "+build_project_name+"\n"
-        else:
-            status_text += "project NOT found! -> needs creating : "+build_project_name+"\n"
-    
-    email_text = (
-        f"1 minute has passed\n"
-        f"so central_codebuild_creator has been triggered\n"
-        f"and the status text is this: "+status_text+"\n"
-        )
-    if (enabled=="yes"):
-        snsclient.publish(TopicArn=topic_arn, Message=email_text, Subject=subjectline)
+for repo in list_of_repos.split(","):
+    build_project_name = build_project_name_start+repo
+    if build_project_name in list_of_project_names:
+        status_text += "project found! : "+build_project_name+"\n"
+    else:
+        status_text += "project NOT found! -> needs creating : "+build_project_name+"\n"
+
+email_text = (
+    f"1 minute has passed\n"
+    f"so central_codebuild_creator has been triggered\n"
+    f"and the status text is this: "+status_text+"\n"
+)
+
+if (enabled=="yes"):
+    snsclient.publish(TopicArn=topic_arn, Message=email_text, Subject=subjectline)
