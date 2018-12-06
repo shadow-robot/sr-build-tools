d466175751bd62a178732f5cb83d2f959766086a
Fotios <fotios@shadowrobot.com>
2018-12-06 11:42:45 +0000

    bug env

--- docker/launch.sh
+++ docker/launch.sh
@@ -442,7 +442,7 @@ if [ ${DESKTOP_ICON} = true ] ; then
 
     echo "Creating launch executable file"
     printf "#! /bin/bash
-    exec -a shadow_launcher_app_xterm xterm -e \"cd ${APP_FOLDER}/${DESKTOP_SHORTCUT_NAME}; ./launch.sh -i ${DOCKER_IMAGE_NAME} -n ${DOCKER_CONTAINER_NAME} -e ${ETHERCAT_INTERFACE} -r false -d false -s true -o ${OPTOFORCE_BRANCH}\"" > ${APP_FOLDER}/${DESKTOP_SHORTCUT_NAME}/shadow_launcher_exec.sh
+    exec -a shadow_launcher_app_xterm xterm -e \"cd ${APP_FOLDER}/${DESKTOP_SHORTCUT_NAME}; ./launch.sh -i ${DOCKER_IMAGE_NAME} -n ${DOCKER_CONTAINER_NAME} -e ${ETHERCAT_INTERFACE} -r false -d false -s true\"" > ${APP_FOLDER}/${DESKTOP_SHORTCUT_NAME}/shadow_launcher_exec.sh
 
     echo "Creating save_ros_logs executable file"
     printf "#! /bin/bash
