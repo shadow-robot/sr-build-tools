739f73454d122b228f682c4ea31715fba11c83e9
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2018-12-11 13:25:34 +0000

    Update build_job_manager.py

--- aws/build_job_manager.py
+++ aws/build_job_manager.py
@@ -2,27 +2,20 @@
 #
 # Copyright (C) 2018 Shadow Robot Company Ltd - All Rights Reserved.
 # Proprietary and Confidential. Unauthorized copying of the content in this file, via any medium is strictly prohibited.
-import github_api
-import aws_api
 
 
 class BuildJobManager(object):
 
-    aws_sns_topic = 'arn:aws:sns:eu-west-2:080653068785:CentralCodeBuildCreatorTopic'
-    build_project_name_start = 'auto--'
-    email_subjectline = 'AWS Lambda for Central AWS CodeBuild Creator'
-    repo_aws_yml_master_branch = 'HEAD'
-    configuration_yml_url = 'https://raw.githubusercontent.com/shadow-robot/sr-build-tools-internal/F%23SRC-2549_aws_central_script_now_supports_yaml/aws/configuration.yml'
-
-    def __init__(self, git_username, git_token):
-        self.git_username = git_username
-        self.git_token = git_token
+    def __init__(self):
+        self.aws_sns_topic = 'arn:aws:sns:eu-west-2:080653068785:CentralCodeBuildCreatorTopic'
+        self.build_project_name_start = 'auto--'
+        self.email_subjectline = 'AWS Lambda for Central AWS CodeBuild Creator'
+        self.repo_aws_yml_master_branch = 'HEAD'
+        self.configuration_yml_url = 'https://raw.githubusercontent.com/shadow-robot/sr-build-tools-internal/F%23SRC-2549_aws_central_script_now_supports_yaml/aws/configuration.yml'
 
     def main(self):
-
-        github_api = GitHubApi(self.git_username, self.git_token)
-
         aws_api = AwsApi()
+        github_api = GitHubApi()
 
         # get repo list in yaml format
 
