91b8bf153658665fabf7067fd1ec246a7eb8c803
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2018-12-11 12:26:32 +0000

    Update build_job_manager.py

--- aws/build_job_manager.py
+++ aws/build_job_manager.py
@@ -6,10 +6,29 @@
 
 class BuildJobManager(object):
 
+    aws_sns_topic = 'arn:aws:sns:eu-west-2:080653068785:CentralCodeBuildCreatorTopic'
+    build_project_name_start = 'auto--'
+    email_subjectline = 'AWS Lambda for Central AWS CodeBuild Creator'
+    repo_aws_yml_master_branch = 'HEAD'
+    configuration_yml_url = 'https://raw.githubusercontent.com/shadow-robot/sr-build-tools-internal/F%23SRC-2549_aws_central_script_now_supports_yaml/aws/configuration.yml'
+
     def __init__(self, git_username, git_token):
         self.git_username = git_username
         self.git_token = git_token
 
     def main(self):
-        #get git credentials
-        print('hello')
+
+        github_api = GitHubApi(self.git_username, self.git_token)
+
+        aws_api = AwsApi()
+
+        # get repo list in yaml format
+
+        (repo_list_yaml_raw, repo_list_response_ok) = github_api.get_file(configuration_yml_url)
+
+        if (repo_list_response_ok):
+            # parse yaml
+            repo_list_yaml = yaml.load(repo_list_yaml_raw)
+            print(repo_list_yaml)
+        else:
+            print('Error getting a repo list from '+configuration_yml_url+" : "+repo_list_yaml_raw)
