7a9553bacdc7965c62515ab85a4a7a93c9fd1c81
Fotios <fotios@shadowrobot.com>
2018-12-11 12:44:49 +0000

    fixing nvidia bug

--- docker/launch.sh
+++ docker/launch.sh
@@ -273,6 +273,13 @@ else
 fi
 
 if [ ${NVIDIA} = true ]; then
+    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+    sudo apt-key add -
+    distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+    curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+    sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+    sudo apt-get update
+    sudo apt-get install -y nvidia-modprobe
     sudo apt-get install -y nvidia-docker
 fi
 
