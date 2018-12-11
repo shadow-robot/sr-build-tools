28bc412614d7c407d0fc88c01d04174eb07b8ffc
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2018-12-11 11:36:39 +0000

    Create aws_api.py

---
+++ aws/aws_api.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2018 Shadow Robot Company Ltd - All Rights Reserved.
+# Proprietary and Confidential. Unauthorized copying of the content in this file, via any medium is strictly prohibited.
+
+
+class AwsApi(object):
+
+    def __init__(self, git_username, git_token):
+        self.git_username = git_username
+        self.git_token = git_token
+
+    def main(self):
+        #get git credentials
+        print('hello')
