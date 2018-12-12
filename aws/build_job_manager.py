2d307388bd90571c461aafa63731e9fad9a71ef3
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2018-12-12 17:45:50 +0000

    Update build_job_manager.py

--- aws/build_job_manager.py
+++ aws/build_job_manager.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-#
 # Copyright (C) 2018 Shadow Robot Company Ltd - All Rights Reserved.
 # Proprietary and Confidential. Unauthorized copying of the content in this file, via any medium is strictly prohibited.
 
@@ -7,23 +5,54 @@
 class BuildJobManager(object):
 
     def __init__(self):
-        self.aws_sns_topic = 'arn:aws:sns:eu-west-2:080653068785:CentralCodeBuildCreatorTopic'
-        self.build_project_name_start = 'auto--'
+        self.sns_topic = 'arn:aws:sns:eu-west-2:080653068785:CentralCodeBuildCreatorTopic'
         self.email_subjectline = 'AWS Lambda for Central AWS CodeBuild Creator'
-        self.repo_aws_yml_master_branch = 'HEAD'
-        self.configuration_yml_url = 'https://raw.githubusercontent.com/shadow-robot/sr-build-tools-internal/F%23SRC-2549_aws_central_script_now_supports_yaml/aws/configuration.yml'
+        self.organisation = 'shadow-robot'
+        self.config_repo = 'sr-build-tools-internal'
+        self.config_branch = 'F#SRC-2549_aws_central_script_now_supports_yaml'
+        self.config_path = 'aws/configuration.yml'
+        self.aws_branch = 'HEAD'
+        self.aws_path = 'aws.yml'
+        self.created_projects = []
+        self.updated_projects = []
+        self.deleted_projects = []
+        self.specified_projects = []
+        self.existing_projects = []
+
+    def generate_projectname(self, repo_name, trunk_name, job_name):
+        project_name = 'auto_'+repo_name+'_'+trunk_name+'_'+job_name
+        return project_name
 
     def main(self):
         aws_api = AwsApi()
         github_api = GitHubApi()
 
-        # get repo list in yaml format
+        self.existing_projects = aws_api.get_projects()
 
-        (repo_list_yaml_raw, repo_list_response_ok) = github_api.get_file(configuration_yml_url)
+        repos_yaml = github_api.get_file(self.organisation, self.config_repo, self.config_branch, self.config_path)
+        repo_list = yaml.load(repos_yaml)['repositories']
 
-        if (repo_list_response_ok):
-            # parse yaml
-            repo_list_yaml = yaml.load(repo_list_yaml_raw)
-            print(repo_list_yaml)
-        else:
-            print('Error getting a repo list from '+configuration_yml_url+" : "+repo_list_yaml_raw)
+        for repo in repo_list:
+            aws_yaml = github_api.get_file(self.organisation, repo, self.aws_branch, self.aws_path)
+            gitrepo = GitRepo(aws_yaml)
+            trunk_names = gitrepo.get_trunks()
+            for trunk_name in trunk_names:
+                job_names = gitrepo.get_jobs(trunk_name)
+                for job_name in job_names:
+                    project_name = self.generate_projectname(repo, trunk_name, job_name)
+                    self.specified_projects.append(project_name)
+                    job = Job(aws_yaml, repo, trunk_name, job_name, project_name)
+                    if (aws_api.project_exists(project_name)):
+                        aws_api.update_project(project_name, job.config)
+                        self.updated_projects.append(project_name)
+                    else:
+                        aws_api.create_project(project_name, job.config)
+                        self.created_projects.append(project_name)
+
+        for project in self.existing_projects:
+            if project not in self.specified_projects:
+                aws_api.delete_project(project)
+                self.deleted_projects.append(project)
+
+        aws_api.send_email(self.email_subjectline, self.aws_sns_topic, self.specified_projects,
+                           self.created_projects, self.updated_projects, self.deleted_projects)
