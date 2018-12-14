2bb06855a74d64b6911de3b5d6f6ff7e38201ed0
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2018-12-14 10:58:06 +0000

    Update gitrepo.py

--- aws/gitrepo.py
+++ aws/gitrepo.py
@@ -1,15 +1,29 @@
-#!/usr/bin/env python
-#
 # Copyright (C) 2018 Shadow Robot Company Ltd - All Rights Reserved.
 # Proprietary and Confidential. Unauthorized copying of the content in this file, via any medium is strictly prohibited.
 
 
 class GitRepo(object):
 
-    def __init__(self, git_username, git_token):
-        self.git_username = git_username
-        self.git_token = git_token
+    def __init__(self, aws_yml):
+        self.raw_yaml = aws_yml
+        self.parsed_yaml = yaml.load(self.raw_yaml)
 
-    def main(self):
-        #get git credentials
-        print('hello')
+    def get_trunks(self):
+        trunk_names = []
+        trunks = self.parsed_yaml['trunks']
+        for trunk in trunks:
+            trunk_name = trunk['name']
+            trunk_names.append(trunk_name)
+        return trunk_names
+
+    def get_jobs(self, trunk_n):
+        job_names = []
+        trunks = self.parsed_yaml['trunks']
+        for trunk in trunks:
+            trunk_name = trunk['name']
+            if (trunk_name == trunk_n):
+                jobs = trunk['jobs']
+                for job in jobs:
+                    job_name = job['name']
+                    job_names.append(job_name)
+        return job_names
