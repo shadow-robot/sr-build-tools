fab7ddfd7c1e154a850aabf69ce06aa2a44dfec8
Fotios <fotios@shadowrobot.com>
2019-01-29 10:33:24 +0000

    removing user tag

--- bin/sr-run-ci-build.sh
+++ bin/sr-run-ci-build.sh
@@ -23,7 +23,7 @@ if  [ "semaphore_docker" != $server_type ] && [ "local" != $server_type ] && [ "
   sudo apt-get update
   sudo apt-get install -y python-dev libxml2-dev libxslt-dev python-pip lcov wget git libssl-dev libffi-dev libyaml-dev
   sudo pip install --upgrade pip setuptools gcovr
-  sudo pip install --user -r ../anisble/data/requirements.txt
+  sudo pip install -r ../anisble/data/requirements.txt
 
   git config --global user.email "build.tools@example.com"
   git config --global user.name "Build Tools"
