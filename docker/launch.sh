5bc0c05fa27a262593ecd88daf3060c42c1aaab4
Tom Queen <tom_q@hotmail.co.uk>
2019-01-31 17:50:40 +0000

    Update launch.sh
    try that

--- docker/launch.sh
+++ docker/launch.sh
@@ -324,11 +324,7 @@ if [ ${NVIDIA} = true ]; then
     sudo apt-get install -y nvidia-modprobe
     sudo apt install -y nvidia-container-runtime
     sudo mkdir -p /etc/systemd/system/docker.service.d
-    sudo tee /etc/systemd/system/docker.service.d/override.conf <<EOF
-    [Service]
-    ExecStart=
-    ExecStart=/usr/bin/dockerd --host=fd:// --add-runtime=nvidia=/usr/bin/nvidia-container-runtime
-    EOF
+
     sudo systemctl daemon-reload
     sudo systemctl restart docker
 
