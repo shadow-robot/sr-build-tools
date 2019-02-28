656be728d56f755604a8f0383ef2ef2e76eb7833
mikramarc <michal@shadowrobot.com>
2019-02-28 15:30:48 +0000

    fixing year

--- ansible/roles/docker/ide/pycharm/files/install_pycharm.sh
+++ ansible/roles/docker/ide/pycharm/files/install_pycharm.sh
@@ -5,7 +5,7 @@ set -e # fail on errors
 
 export PYCHARM_HOME=${1:-/etc/pycharm}
 
-wget https://download-cf.jetbrains.com/python/pycharm-community-2017.3.5.tar.gz -O /tmp/pycharm.tar.gz
+wget https://download-cf.jetbrains.com/python/pycharm-community-2018.3.5.tar.gz -O /tmp/pycharm.tar.gz
 sudo mkdir ${PYCHARM_HOME}
 tar -xzvf /tmp/pycharm.tar.gz -C ${PYCHARM_HOME} --strip=1
 wget -P /tmp/ https://bootstrap.pypa.io/get-pip.py
