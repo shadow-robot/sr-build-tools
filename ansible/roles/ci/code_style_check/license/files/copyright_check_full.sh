f505b8040db2b1a6129aa45fc5fbf5ca2cc3e282
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2019-07-22 17:42:38 +0100

    Update copyright_check_full.sh

--- ansible/roles/ci/code_style_check/license/files/copyright_check_full.sh
+++ ansible/roles/ci/code_style_check/license/files/copyright_check_full.sh
@@ -3,46 +3,63 @@
 export directory=$1
 cd $directory
 
-declare -A copyright_array
-declare -A exclusion_array
+filetypes=(py c h cpp hpp)
 
-file_extensions=(py c h cpp hpp)
+copyrights_py=("# Copyright")
+exclusions_py=("__init__" "setup.py")
 
-copyright_array[py]="# Copyright"
-exclusion_array[py]="__init__,setup.py"
+copyright_symbol_utf16=$'\x20\xa9'
+copyright_symbol_utf8=$'\xc2\xa9'
 
-copyright_array[c]="// ©,//©,//Copyright,// Copyright"
-exclusion_array[c]=""
+copyrights_c=("// "$copyright_symbol_utf16 "//"$copyright_symbol_utf16 "// "$copyright_symbol_utf8 "//"$copyright_symbol_utf8)
+exclusions_c=()
 
-copyright_array[h]="// ©,//©,//Copyright,// Copyright"
-exclusion_array[h]=""
+copyrights_h=("// "$copyright_symbol_utf16 "//"$copyright_symbol_utf16 "// "$copyright_symbol_utf8 "//"$copyright_symbol_utf8)
+exclusions_h=()
 
-copyright_array[cpp]="// ©,//©,//Copyright,// Copyright"
-exclusion_array[cpp]=""
+copyrights_cpp=("// "$copyright_symbol_utf16 "//"$copyright_symbol_utf16 "// "$copyright_symbol_utf8 "//"$copyright_symbol_utf8)
+exclusions_cpp=()
+copyrights_hpp=("// "$copyright_symbol_utf16 "//"$copyright_symbol_utf16 "// "$copyright_symbol_utf8 "//"$copyright_symbol_utf8)
+exclusions_hpp=()
 
-copyright_dict[hpp]="// ©,//©,//Copyright,// Copyright"
-exclusion_dict[hpp]=""
-
-for file_extension in "${file_extensions[@]}"; do
-    for file_name in `find . -name "*.$file_extension" -type f`; do
-        [[ "${exclusion_dict[$file_extension]}" =~ (^|[[:space:]])$x($|[[:space:]]) ]] && echo 'yes' || echo 'no'
-        # Ignoring some pre-defined files 
-        if [[ " ${exclusion_dict[*]} " == *" d "* ]]; then
-        if [[ $file_name != *"__init__"* ]] && [[ $file_name != *"setup.py"* ]]; then
-            copyright_line=$(grep "$copyright_str_python" $file_name)
-        if [ "$copyright_line" == "" ]; then
-            if [ $num_files_no_copyright_python == 0 ]; then
-                echo "The following python files do not have copyright:"
+has_missing_copyrights=false
+total_num_files_no_copyright=0
+for filetype in "${filetypes[@]}"; do
+    num_files_no_copyright=0
+    exclusions_name="exclusions_$filetype"[@]
+    exclusions=("${!exclusions_name}")
+    copyrights_name="copyrights_$filetype"[@]
+    copyrights=("${!copyrights_name}")
+    for filename in $(find . -name "*.$filetype" -type f); do
+        accept_file=true
+	for exclusion in "${exclusions[@]}"; do
+	    if [[ $(echo -n $exclusion | wc -m) > 0 ]] && [[ $filename == *$exclusion* ]] ; then
+	        accept_file=false
             fi
-            echo $file_name
-            let "num_files_no_copyright_python++"
+        done
+	has_copyright=false
+	if $accept_file; then
+	    has_copyright=false
+	    for copyright in "${copyrights[@]}"; do
+	        copyright_line=$(grep -a "$copyright" "$filename")
+		if [[ $(echo -n $copyright_line | wc -m) > 0 ]]; then
+		    has_copyright=true
+		fi
+	    done
+	    if ! $has_copyright; then
+	        echo $'\n'"$filename"
+		(( num_files_no_copyright++ ))
+		(( total_num_files_no_copyright++ ))
+		has_missing_copyrights=true
+	    fi
         fi
+    done
+    if [ $num_files_no_copyright != 0 ]; then
+        echo $'\n'"Copyright check failure: There are $num_files_no_copyright $filetype files without copyright. See above for a list of files"
     fi
 done
-
-if [ $num_files_no_copyright != 0 ]; then
-    echo "There is a total of $num_files_no_copyright files without copyright"
+if $has_missing_copyrights; then
+    echo $'\n\n'"Copyright check failure: There are $total_num_files_no_copyright files in total without copyright. See above for details."
     exit 1
 fi
-echo "All files have copyright"
 exit 0
