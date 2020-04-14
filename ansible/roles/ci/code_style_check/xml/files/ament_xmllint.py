7722f45cbe859f5fb02d08e681d8471acfd7419a
ToivoS <toivo@shadowrobot.com>
2020-04-14 13:23:16 +0100

    ament_xmllint executed with python3

--- ansible/roles/ci/code_style_check/xml/files/ament_xmllint.py
+++ ansible/roles/ci/code_style_check/xml/files/ament_xmllint.py
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import argparse
 import os
 import shutil
@@ -68,7 +67,6 @@ def main(argv=sys.argv[1:]):
     # invoke xmllint on all files
     for filename in files:
         # parse file to extract desired validation information
-        print("debug: " + str(filename))
         parser = make_parser()
         handler = CustomHandler()
         parser.setContentHandler(handler)
