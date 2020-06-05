ae9dc8889659cb32da5fe282debde4fa6306cb99
ToivoS <38542384+ToivoS@users.noreply.github.com>
2020-06-05 15:39:24 +0100

    Update license_check.sh

--- ansible/roles/ci/code_style_check/license/files/license_check.sh
+++ ansible/roles/ci/code_style_check/license/files/license_check.sh
@@ -3,15 +3,15 @@
 export directory=$1
 cd $directory
 
-if [ $(find . -name "LICENSE" | wc -l) == 0 ]; then
+if [ $(find . -maxdepth 1 -name "LICENSE" | wc -l) == 0 ]; then
     echo "No license file present"
     exit 1
 fi
-name=$(find . -name "LICENSE")
+name=$(find . -maxdepth 1 -name "LICENSE")
 if [ $(wc -c < "$name") -le 235 ]; then
-    echo -e "License file present but content is different than expected. Please use the following text: \n"
-    echo -e "Copyright (C) 2017 Shadow Robot Company Ltd - All Rights Reserved."
-    echo -e "Redistribution and use in source and binary forms, with or without modification, are strictly prohibited by anyone without specific permission from Shadow Robot Company.\n"
+    echo -e "License file present but content is different than expected.\n"
+    echo $'\n'"Our License templates are here:"
+    echo "https://shadowrobot.atlassian.net/wiki/spaces/SDSR/pages/594411521/Licenses"
     exit 1
 fi
 echo "License file present"
