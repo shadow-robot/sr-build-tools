f8466ba7d7235b4c5fb05f0f6b44055ea02e4563
Ethan Fowler <ethan@shadowrobot.com>
2020-09-22 14:37:39 +0000

    Working binarize script to /opt/ros/shadow

--- bin/binarize.sh
+++ bin/binarize.sh
@@ -3,7 +3,7 @@
 set -e
 
 workspace_path=$1
-# pyarmor_license_zip_file_path=$2
+pyarmor_license_zip_file_path=$2
 user_name=${4:-user}
 install_space=${5:-/opt/ros/shadow}
 underlay_devel=${3:-$install_space}
@@ -12,20 +12,20 @@ source $workspace_path/devel/setup.bash
 
 echo "Running binarization script on workspace: $workspace_path"
 
-# echo "Installing pyarmor"
-# apt update
-# apt install python-pip
-# pip install pyarmor
-# pyarmor register $pyarmor_license_zip_file_path
-# pyarmor runtime --output "/opt/ros/$ROS_DISTRO/lib/python2.7/dist-packages"
-# if [ -f $HOME/.pyarmor_capsule.zip ]; then
-#     rm $HOME/.pyarmor_capsule.zip
-# fi
+echo "Installing pyarmor"
+apt update
+apt install python-pip
+pip install pyarmor
+pyarmor register $pyarmor_license_zip_file_path
+pyarmor runtime --output "/opt/ros/$ROS_DISTRO/lib/python2.7/dist-packages"
+if [ -f $HOME/.pyarmor_capsule.zip ]; then
+    rm $HOME/.pyarmor_capsule.zip
+fi
 
-# echo "Building with catkin_make_isolated"
-# cd $workspace_path
-# catkin_make_isolated
-# cd
+echo "Building with catkin_make_isolated"
+cd $workspace_path
+catkin_make_isolated
+cd
 
 echo "finding all repos in workspace"
 list_of_repos=()
@@ -99,7 +99,7 @@ catkin_make_isolated --install --install-space $install_space --only-pkg-with-de
 mv ./devel-backup ./devel
 mv ./build-backup ./build
 
-# echo "Removing build and devel directories"
+echo "Removing build and devel directories"
 rm -rf ./devel ./build ./devel_isolated ./build_isolated
 
 echo "Removing source code"
@@ -110,76 +110,76 @@ do
    wstool remove $repo
 done
 
-# echo "Removing headers if they were installed"
-# cd $install_space/include
-# for package in "${list_of_private_packages[@]}"
-# do
-#    if [ ! -d "$package" ]; then
-#       continue
-#    fi
-#    rm -rf "$package"
-# done
+echo "Removing headers if they were installed"
+cd $install_space/include
+for package in "${list_of_private_packages[@]}"
+do
+   if [ ! -d "$package" ]; then
+      continue
+   fi
+   rm -rf "$package"
+done
 
 echo "Building public packages"
 cd $workspace_path
 source $underlay_devel/setup.bash
 gosu $user_name catkin_make -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
-# echo "Running pyarmorize"
-# pyarmorize_paths=("$install_space/lib" "$install_space/lib/python2.7/dist-packages")
-# for pyarmorize_path in "${pyarmorize_paths[@]}"
-# do
-#    cd $pyarmorize_path
-#    for package in "${list_of_private_packages[@]}"
-#    do
-#       if [ ! -d "$package" ]; then
-#          continue
-#       fi
+echo "Running pyarmorize"
+pyarmorize_paths=("$install_space/lib" "$install_space/lib/python2.7/dist-packages")
+for pyarmorize_path in "${pyarmorize_paths[@]}"
+do
+   cd $pyarmorize_path
+   for package in "${list_of_private_packages[@]}"
+   do
+      if [ ! -d "$package" ]; then
+         continue
+      fi
 
-#       python_files_no_py_extension=()
-#       cd $package
+      python_files_no_py_extension=()
+      cd $package
 
-#       python_files_no_py_extension+=($(find . -type f -not -name '*.py' -exec grep -R -I -P '^#!/usr/bin/env python|^#! /usr/bin/env python|^#!/usr/bin/python|^#! /usr/bin/python' -l {} \; | sed "s/\.\///g"))
+      python_files_no_py_extension+=($(find . -type f -not -name '*.py' -exec grep -R -I -P '^#!/usr/bin/env python|^#! /usr/bin/env python|^#!/usr/bin/python|^#! /usr/bin/python' -l {} \; | sed "s/\.\///g"))
       
-#       for file in "${python_files_no_py_extension[@]}"
-#       do
-#          mv $file "$file.py"
-#       done
+      for file in "${python_files_no_py_extension[@]}"
+      do
+         mv $file "$file.py"
+      done
 
-#       if [ -f "__init__.py" ]; then
-#          mkdir tmp_init_file_dir
-#          mv __init__.py ./tmp_init_file_dir
-#       fi
+      if [ -f "__init__.py" ]; then
+         mkdir tmp_init_file_dir
+         mv __init__.py ./tmp_init_file_dir
+      fi
 
-#       python_files_in_current_dir=`ls -1 *.py 2>/dev/null | wc -l`
-#       if [ $python_files_in_current_dir -eq 0 ]
-#       then
-#          if [ -d "tmp_init_file_dir" ]; then
-#             mv ./tmp_init_file_dir/__init__.py .
-#             rm -rf ./tmp_init_file_dir
-#          fi
-#          cd ..
-#          continue
-#       fi 
+      python_files_in_current_dir=`ls -1 *.py 2>/dev/null | wc -l`
+      if [ $python_files_in_current_dir -eq 0 ]
+      then
+         if [ -d "tmp_init_file_dir" ]; then
+            mv ./tmp_init_file_dir/__init__.py .
+            rm -rf ./tmp_init_file_dir
+         fi
+         cd ..
+         continue
+      fi 
 
-#       pyarmor obfuscate --exact --no-runtime *.py
-#       rm *.py
-#       mv ./dist/* .
-#       rm -rf dist
-#       chmod +x *.py
+      pyarmor obfuscate --exact --no-runtime *.py
+      rm *.py
+      mv ./dist/* .
+      rm -rf dist
+      chmod +x *.py
 
-#       if [ -d "tmp_init_file_dir" ]; then
-#          mv ./tmp_init_file_dir/__init__.py .
-#          rm -rf ./tmp_init_file_dir
-#       fi
+      if [ -d "tmp_init_file_dir" ]; then
+         mv ./tmp_init_file_dir/__init__.py .
+         rm -rf ./tmp_init_file_dir
+      fi
 
-#       for file in "${python_files_no_py_extension[@]}"
-#       do
-#          mv "$file.py" $file
-#       done
-#       cd ..
-#    done
-# done
+      for file in "${python_files_no_py_extension[@]}"
+      do
+         mv "$file.py" $file
+      done
+      cd ..
+   done
+done
 
 echo "Binarize: done."
 
