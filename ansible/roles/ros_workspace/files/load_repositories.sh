b96bc088e23afce7e98fc293b1147a5a7ab0e4db
maxzieba <max@shadowrobot.com>
2021-03-05 15:56:04 +0000

    Fixed to shallow cloning for repositories

--- ansible/roles/ros_workspace/files/load_repositories.sh
+++ ansible/roles/ros_workspace/files/load_repositories.sh
@@ -19,6 +19,27 @@ recursive_rosinstall () {
     done
 }
 
+recursive_rosinstall_shallow () {
+    while [ $current_repo_count -ne $previous_repo_count ]; do
+        find $current_folder -type f -name $rosinstall_filename -exec wstool merge -y {} \;
+        sed -i "$1" .rosinstall
+
+        mv .rosinstall repository.rosinstall
+  	wstool init --shallow . repository.rosinstall -j5
+
+        export previous_repo_count=$current_repo_count
+        export current_repo_count=$(find $destination_folder -type f -name $rosinstall_filename | wc -l)
+
+        if [ $loops_count -ge 1 ]; then
+            export loops_count=$((loops_count - 1))
+        else
+            break
+        fi
+        export current_folder=$destination_folder
+    done
+}
+
+
 export initial_folder=$1
 export destination_folder=$2
 export levels_depth=$3
@@ -38,7 +59,7 @@ export previous_repo_count=-1
 export loops_count=$((levels_depth - 1))
 
 if [ "${use_ssh_uri}" = true ]; then
-    recursive_rosinstall "/https/s/\//:/3; s/https:\/\/{{github_login}}:{{github_password}}/git/g; s/https:\/\//git@/g"
+    recursive_rosinstall_shallow "/https/s/\//:/3; s/https:\/\/{{github_login}}:{{github_password}}/git/g; s/https:\/\//git@/g"
 else
-    recursive_rosinstall "s/{{github_login}}/$github_user/g; s/{{github_password}}/$github_password/g"
+    recursive_rosinstall_shallow "s/{{github_login}}/$github_user/g; s/{{github_password}}/$github_password/g"
 fi
