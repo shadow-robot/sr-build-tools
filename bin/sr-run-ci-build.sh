c30a1abf0e56e94c5019c1727b1abe9e50159005
Fotios Papadopoulos <fotios@shadowrobot.com>
2021-04-28 13:09:36 +0100

    Update sr-run-ci-build.sh

--- bin/sr-run-ci-build.sh
+++ bin/sr-run-ci-build.sh
@@ -23,8 +23,7 @@ if  [ "semaphore_docker" != $server_type ] && [ "local" != $server_type ] && [ "
   sudo apt-get update
   sudo apt-get install -y python3-dev libxml2-dev libxslt-dev python3-pip lcov wget git libssl-dev libffi-dev libyaml-dev
   sudo pip3 install --upgrade pip setuptools==44.0.0 gcovr
-  sudo pip3 install -U pycodestyle cpplint
-  
+ 
   git config --global user.email "build.tools@example.com"
   git config --global user.name "Build Tools"
 
