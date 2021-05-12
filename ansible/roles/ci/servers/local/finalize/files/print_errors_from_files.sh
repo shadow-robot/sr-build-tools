f0e90599367796fb4d65293b4da5d7a5ee7ab830
Fotios Papadopoulos <fotios@shadowrobot.com>
2021-05-12 22:30:09 +0100

    Update print_errors_from_files.sh

--- ansible/roles/ci/servers/local/finalize/files/print_errors_from_files.sh
+++ ansible/roles/ci/servers/local/finalize/files/print_errors_from_files.sh
@@ -21,7 +21,7 @@ for file_path in $(find . -type f); do
             new_error_line=$(echo ${lines_in_cdata[$i]} | grep -Po '^/.*')
             if [[ ! -z ${new_error_line} ]]; then
                 (( error_count++ ))
-                cleaned_up_error=$(echo ${lines_in_cdata[$i]} | sed 's/\/home.*shadow-robot\///' | sed 's/\*.*//' )
+                cleaned_up_error=$(echo ${lines_in_cdata[$i]} | sed 's/\/home.*shadow-robot\///' | sed 's/\*\*\*\*\*\*.*//' )
                 echo -e "\nError $error_count in $cleaned_up_error"
             else
                 if [[ -z ${done_processing} ]] && [[ -z ${total_errors_found} ]]; then
