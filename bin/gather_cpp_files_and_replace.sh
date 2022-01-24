ee02bc9cd1e5782eff2eadc356b8fa9aa09f6fa6
Ben <86227623+BenStarmerSmith@users.noreply.github.com>
2022-01-24 14:48:00 +0000

    Src 6823 enable licencing for dev (#624)
    * Added code to find all private repos in projects/shadow_robot/base/src, look through all the cpp files, and switch the licencing flags from false to true.
    
    * Takes folder as input now, not fixed
    
    * Update gather_cpp_files_and_replace.sh
    
    * Changed the replace value to search for a const bool instead of just a bool
    
    * Updated the script to look for the new licence cehck
    
    * Added escape char to allow for whole text to be replaced

---
+++ bin/gather_cpp_files_and_replace.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+list_of_private_repos=()
+
+Gather_Private_Repos () {
+    cwd= pwd
+    cd $1
+    for repo in $(find . -maxdepth 1 -type d -printf '%f\n') 
+    do
+        repo="$1/$repo"
+        cd $repo
+        if [ -d ".git" ]
+        then
+            repo_https_url=$(git remote -v | awk '{print $2}' | sed 's/git@github.com:/https:\/\/github.com\//g' | head -n 1 | sed 's/\.git//g')
+            return_code=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $repo_https_url)
+            if [ $return_code -ne 200 ]
+            then
+                list_of_private_repos+=($repo)
+            fi
+        fi
+    done
+    cd $cwd
+}
+
+Find_And_Replace_Licence_Var () {
+    directories=( $(find $1 -type f -name "*.cpp") )
+    for dir in ${directories[@]} 
+    do
+        sed -i 's/\"if (false && licence_check() != 0) exit(1);\"/\"if (true && licence_check() != 0) exit(1);\"/g' $dir
+    done
+}
+
+Gather_Private_Repos $1
+for repo in ${list_of_private_repos[@]}
+do
+    Find_And_Replace_Licence_Var $repo
+done
