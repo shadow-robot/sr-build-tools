36bbab65507a397826ced23e2bdbb58e73cb61de
Ben Starmer-Smith <ben@shadowrobot.com>
2022-05-10 10:37:04 +0100

    Removed ansible uninstall

--- bin/sr-run-ci-build.sh
+++ bin/sr-run-ci-build.sh
@@ -42,7 +42,6 @@ if  [ "semaphore_docker" != $server_type ] && [ "local" != $server_type ] && [ "
     git clone --depth 1 https://github.com/shadow-robot/sr-build-tools.git -b "$toolset_branch" $build_tools_folder
     cd $build_tools_folder/ansible
   fi
-  sudo pip3 uninstall -y ansible-base ansible-core ansible
   sudo pip3 install -r data/requirements.txt
 fi
 
