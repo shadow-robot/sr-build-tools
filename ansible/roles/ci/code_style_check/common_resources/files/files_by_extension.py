3c5376c62b3ed00300f41b15fa6f91e79a7b78a3
Ben Starmer-Smith <ben@shadowrobot.com>
2023-01-04 11:04:12 +0000

    updated year.

--- ansible/roles/ci/code_style_check/common_resources/files/files_by_extension.py
+++ ansible/roles/ci/code_style_check/common_resources/files/files_by_extension.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2022 Shadow Robot Company Ltd.
+# Copyright 2022-2023 Shadow Robot Company Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
