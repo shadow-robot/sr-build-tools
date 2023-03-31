c4240248d33e7ab95ba8098b6c0b5560dce28fdd
Ethan Fowler <ethan@shadowrobot.com>
2023-03-31 17:59:45 +0000

    Adding one line installer for the short term

---
+++ ansible/roles/ros_workspace/files/vs_code/one_line_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+build_tools_dir=~/sr-build-tools-test
+build_tools_branch=F_#SP-476_vs_code_setup
+rm -rf $build_tools_dir
+git clone --depth 1 --single-branch -b $build_tools_branch https://github.com/shadow-robot/sr-build-tools.git $build_tools_dir
+cp -r $build_tools_dir/ansible/roles/ros_workspace/files/vs_code/.vscode ~/projects/shadow_robot/base/.
+echo "Run 'ROS: Update C++ Properties' command in VS Code to make C++ intellisense etc. work."
