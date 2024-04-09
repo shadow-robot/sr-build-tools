23e4d41d26ecb27ff0e6c8e4d691aa973cd0c00e
George E-Green <112101154+Athen-Player1@users.noreply.github.com>
2024-04-09 11:27:24 +0100

    wget failing due to ssl cert fix

--- bin/prepare_rt_kernel.sh
+++ bin/prepare_rt_kernel.sh
@@ -6,8 +6,8 @@ KERNEL_DIR="v4.x"
 KERNEL_VERSION="4.13.13"
 
 # Double check the links here as they do change over time to include new paths i.e. kernel/projects/old/rt
-wget https://www.kernel.org/pub/linux/kernel/projects/rt/${RT_PATCH_DIR}/patch-${RT_PATCH_VERSION}.patch.xz  --no 
-wget https://www.kernel.org/pub/linux/kernel/${KERNEL_DIR}/linux-${KERNEL_VERSION}.tar.xz
+wget https://www.kernel.org/pub/linux/kernel/projects/rt/${RT_PATCH_DIR}/patch-${RT_PATCH_VERSION}.patch.xz  --no-check-certificate
+wget https://www.kernel.org/pub/linux/kernel/${KERNEL_DIR}/linux-${KERNEL_VERSION}.tar.xz --no-check-certificate
 tar -xvJf linux-${KERNEL_VERSION}.tar.xz
 cd linux-${KERNEL_VERSION}/
 xzcat -dc ../patch-${RT_PATCH_VERSION}.patch.xz | patch -p1
