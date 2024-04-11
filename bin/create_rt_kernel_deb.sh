67c3a8a9a0464411fe2d5bb0aae98d10268847a4
dg-shadow <dg@shadowrobot.com>
2024-04-11 14:38:47 +0100

    Revert "Fix create+prepare rt kernel scripts (#701)"
    This reverts commit aa368eb0378131bfad8717cb15e8711f32bace83.

--- bin/create_rt_kernel_deb.sh
+++ bin/create_rt_kernel_deb.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-# 1) Replace with kernel version
-KERNEL_VERSION="x.x.x"
+
+KERNEL_VERSION="4.13.13"
 
 # https://www.debian.org/releases/stable/i386/ch08s06.html.en
-sudo apt-get install kernel-package fakeroot libssl-dev dpkg-dev libelf-dev
+sudo apt-get install kernel-package fakeroot libssl-dev
 
 cd linux-${KERNEL_VERSION}/
-make clean
-
+make-kpkg clean
 # set concurrency to all cores but one
 export CONCURRENCY_LEVEL=$(expr $(grep -c ^processor /proc/cpuinfo) - 1)
-
-# 2)a) on systems prior to 22.04 run below
-#sudo fakeroot make-kpkg --initrd --revision=1.0.custom kernel_image
-
-#2)b) on systems 22.04 and up run
-#sudo fakeroot make bindeb-pkg
+sudo fakeroot make-kpkg --initrd --revision=1.0.custom kernel_image
--- bin/prepare_rt_kernel.sh
+++ bin/prepare_rt_kernel.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-RT_PATCH_DIR="x.xx"
-RT_PATCH_VERSION="x.xx.xx-rtx"
-KERNEL_DIR="vx.x"
-KERNEL_VERSION="x.xx.xx"
+RT_PATCH_DIR="4.13"
+RT_PATCH_VERSION="4.13.13-rt5"
+KERNEL_DIR="v4.x"
+KERNEL_VERSION="4.13.13"
 
-# Double check the links here as they do change over time to include new paths i.e. kernel/projects/old/rt
-wget https://www.kernel.org/pub/linux/kernel/projects/rt/${RT_PATCH_DIR}/patch-${RT_PATCH_VERSION}.patch.xz  --no-check-certificate
-wget https://www.kernel.org/pub/linux/kernel/${KERNEL_DIR}/linux-${KERNEL_VERSION}.tar.xz --no-check-certificate
+wget https://www.kernel.org/pub/linux/kernel/projects/rt/${RT_PATCH_DIR}/patch-${RT_PATCH_VERSION}.patch.xz
+wget https://www.kernel.org/pub/linux/kernel/${KERNEL_DIR}/linux-${KERNEL_VERSION}.tar.xz
 tar -xvJf linux-${KERNEL_VERSION}.tar.xz
 cd linux-${KERNEL_VERSION}/
 xzcat -dc ../patch-${RT_PATCH_VERSION}.patch.xz | patch -p1
@@ -19,9 +18,6 @@ make mrproper
 cp /boot/config-`uname -r` .config
 
 make olddefconfig
-# Setting systemkeys to avoid; No rule to make target 'debian/certs/test-signing-certs.pem' build error
-scripts/config --set-str SYSTEM_TRUSTED_KEYS ""
-scripts/config --disable SYSTEM_REVOCATION_KEYS
 
 # edit manually the RT_PREEMPT parameters:
 # General Setup -> Preemption Model  set to Fully Preemptible Kernel (RT)
