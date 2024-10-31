5debad6453ef251f55d071e13b93c0f91ab57c7f
Tom Queen <tom_q@hotmail.co.uk>
2024-10-31 12:41:29 +0000

    Update sr_quality_of_life.sh

--- bin/sr_quality_of_life.sh
+++ bin/sr_quality_of_life.sh
@@ -32,10 +32,27 @@ if [ -z "${BASH_ONLY}" ]; then
 	BASH_ONLY=false
 fi
 
+echo ""
+echo "================================================================="
+echo "|                                                               |"
+echo "|                 Shadow quality of life tools                  |"
+echo "|                                                               |"
+echo "================================================================="
+echo ""
+echo "possible options: "
+echo "  * -u or --user                Overwrite the auto-detected username (current username is $MY_USERNAME)"
+echo "  * -b or --bash_only           Set to true to only install bash functions"
+echo ""
+echo "example:  bash <(curl -Ls bit.ly/sr_qol)"
+echo ""
+echo "User?             = ${MY_USERNAME}"
+echo "BASH_ONLY?        = ${BASH_ONLY}"
+
+
 if [[ "${BASH_ONLY}" == false ]]; then
 
     echo "Installing and configuring additional quality-of-life tools"
-    sudo apt install -y tree highlight speedometer xsel screen
+    sudo apt install -y tree highlight speedometer xsel screen nano git curl jq nmap
 
     echo "Configuring highlight"
     for new_lang in $(echo -e "launch\nxacro\nurdf"); do
@@ -49,9 +66,9 @@ if [[ "${BASH_ONLY}" == false ]]; then
     /home/${MY_USERNAME}/.fzf/install --all
 fi
 
-
-echo "Grabbing additional bash cmds"
-wget -O /home/${MY_USERNAME}/.bash_functions https://raw.githubusercontent.com/shadow-robot/sr-build-tools/F_add_useful_bash_stuff/docker/utils/additional_bashrc_commands_quality_of_life
+BASH_FUNCTIONS_LOCATION="/home/${MY_USERNAME}/.bash_functions"
+echo "Grabbing additional bash cmds and saving them to ${BASH_FUNCTIONS_LOCATION}"
+wget -O ${BASH_FUNCTIONS_LOCATION} https://raw.githubusercontent.com/shadow-robot/sr-build-tools/F_add_useful_bash_stuff/docker/utils/additional_bashrc_commands_quality_of_life
 if [[ $(cat /home/${MY_USERNAME}/.bashrc  | grep "source ~/.bash_functions" | wc -l) -eq 0 ]]; then
     echo "source ~/.bash_functions" >> /home/${MY_USERNAME}/.bashrc
 fi
