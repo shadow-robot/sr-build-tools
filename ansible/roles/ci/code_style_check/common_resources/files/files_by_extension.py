shadow-robot/sr-build-tools

commit e9eb50ee7f7a4fa0bf9e8345f69b34c806272c38

Added the ability to skip lint checks at folder level (#670)
* Added a way of skipping c files from linter checks, extended it to also work with python. Added docs.

* Fixed brices comment.

* Fixed error in python check.

* Added back try except.

* Addressed max's comments.

* Removed unused arg.

* Fixed literals.

* Replaced python file headers with rejex

* Added Max's suggestion.

* updated year.

ansible/roles/ci/code_style_check/common_resources/files/files_by_extension.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2022 Shadow Robot Company Ltd.
+# Copyright 2022-2023 Shadow Robot Company Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
@@ -15,13 +15,13 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+import re
 import os
 import argparse
 
 
+LINT_IGNORE_FILE = "lint_exclusions.cfg"
 CPP_HEADERS = [".cpp", ".c", ".h", ".hpp"]
-PYTHON_HEADERS = ["#!/usr/bin/env python", "#! /usr/bin/env python", "#!/usr/bin/python", "#! /usr/bin/python",
-                       "#!/usr/bin/env python3", "#! /usr/bin/env python3", "#!/usr/bin/python3", "#! /usr/bin/python3"]
 
 
 def argparser():
@@ -37,29 +37,75 @@ def argparser():
 
 
 def gather_all_python_files(file_path):
+    """This function walks through the given path collects all python files (with and without extension based on
+       header). It will exclude all python files found in the lint ignore file."""
     output = []
     for dirpath, _, filenames in os.walk(file_path):
+        excluded_files = gather_excluded_files(dirpath)  # Gathers all the files to be ignored in this folder
         for filename in filenames:
-            file_path = f"{dirpath}/{filename}"
-            try:
-                fline = ""
-                with open(file_path) as python_file:
-                    fline = python_file.readline().strip()
-                if any(fline in head for head in PYTHON_HEADERS) and fline != "" and fline != "#":
-                    output.append(file_path)
-            except Exception:  # We don't actually care about the exception as it wont fail on python files.
+            if filename in excluded_files:  # Skip excluded files
                 continue
+            full_file_path = os.path.join(dirpath, filename)
+            valid_file = check_is_python_file(full_file_path)
+            if valid_file:
+                output.append(full_file_path)
     return output
 
 
 def gather_all_cpp_files(file_path):
+    """This function walks through the given path collects all c, cpp, h, hpp files It will exclude all python files
+       found in the lint ignore file."""
     output = []
     for dirpath, _, filenames in os.walk(file_path):
+        excluded_files = gather_excluded_files(dirpath)  # Gathers all the files to be ignored in this folder
         for filename in filenames:
+            if filename in excluded_files:  # Skip excluded files
+                continue
             if os.path.splitext(filename)[1] in CPP_HEADERS:
-                output.append(f"{dirpath}/{filename}")
+                output.append(os.path.join(dirpath, filename))
     return output
 
+def gather_excluded_files(folder_path):
+    """Gatheres all of the files to ignore in the lint ignore file (if it exists). If the file contains
+       `exclude_files=*` then all files will be ignored, or you can list indiviual files like
+       `exclude_files=test.py,hello.cpp,hi.h`."""
+    folder_files = [folder_file for folder_file in os.listdir(folder_path)
+                    if os.path.isfile(os.path.join(folder_path, folder_file))]
+    folder_files_filtered = []
+
+    if LINT_IGNORE_FILE in folder_files:  # Check there are actually files to skip
+        for folder_file in folder_files:
+            if check_is_python_file(os.path.join(folder_path, folder_file)):
+                folder_files_filtered.append(folder_file)
+            if os.path.splitext(folder_file)[1] in CPP_HEADERS:
+                folder_files_filtered.append(folder_file)
+
+        re_pattern = re.compile(r'exclude_files=((?:[^,]*,)*[^,]*)')
+        with open(os.path.join(folder_path, LINT_IGNORE_FILE)) as ignore_file:
+            content = ignore_file.readline()
+
+        excluded_files = re_pattern.search(content).group(1)
+        if "*" in excluded_files:  # Tells us to skip all files
+            return folder_files_filtered
+        return [folder_file.strip() for folder_file in excluded_files.split(",")]
+
+    return []
+
+def check_is_python_file(file_path):
+    """This function is used to check if a file is a python file. It first checks the extension, if this doesn't return
+       .py then it will check the first line of the file against the python headers. Returns true if the file is a
+       python file."""
+    if os.path.splitext(file_path)[1] == ".py":
+        return True
+
+    fline = ""
+    try:  # Used to catch files that won't open or have the wrong encoding type.
+        with open(file_path) as python_file:
+            fline = python_file.readline().strip()
+    except:
+        pass
+    finally:
+        return re.match(r'#!\s*/usr/bin/(env|python|python3) (python|python3)', fline)
 
 if __name__ == "__main__":
     path, filetype = argparser()

commit 72e78e67a6f0ba756fff580fc71da2dfc1f8b0e3

Fix regex to detect python files without extension. This got broken in commit e9eb50e when regex was introduced.

ansible/roles/ci/code_style_check/common_resources/files/files_by_extension.py
@@ -105,7 +105,7 @@ def check_is_python_file(file_path):
     except:
         pass
     finally:
-        return re.match(r'#!\s*/usr/bin/(env|python|python3) (python|python3)', fline)
+        return re.match(r'#!\s*/usr/bin/(env|python|python3)( (python|python3))*', fline)
 
 if __name__ == "__main__":
     path, filetype = argparser()